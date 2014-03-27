af8150cdfe03a07bd1e2ee69fadc08193426d2c9
ahhurlbert <hurlbert@bio.unc.edu>
2014-03-26 22:33:07 -0400

    added calculation of more summary stats including number of unique sites, number of unique species, number of unique times in the entire dataset, as well as mean richness per site, number of time samples per site, and finally, number of sites and times per species

--- dataset_summary_fun.r
+++ dataset_summary_fun.r
@@ -4,7 +4,26 @@ ct_summary = function(dataset, siteID, spID, timeID) {
   
   data = unique(dataset[, c(siteID, spID, timeID)])
   
-  # Mean and cumulative richness per site
-  site.rich = data.frame(table(data[, c(spID, timeID)]))
-  site.rich = site.rich[site.rich$Freq != 0, ]
+  num.sites = length(unique(dataset[, siteID]))
+  num.spp = length(unique(dataset[, spID]))
+  num.times = length(unique(dataset[, timeID]))
+  
+  # Mean richness per site
+  site.yr.rich = data.frame(table(data[, c(timeID, siteID)]))
+  site.yr.rich = site.yr.rich[site.yr.rich$Freq != 0, ]
+  mean.site.rich = aggregate(site.yr.rich$Freq, by = list(site.yr.rich$stateroute), mean)
+  names(mean.site.rich) = c(siteID, 'Richness')
+  
+  # Time samples per site
+  site.times = unique(dataset[, c(siteID, timeID)])
+  site.time.count = data.frame(table(site.times[, siteID]))
+  names(site.time.count) = c(siteID, 'Times')
+  
+  # Number of sites and number of years per species
+  sp.sites = unique(dataset[, c(siteID, spID)])
+  sp.site.count = data.frame(table(sp.sites[, spID]))
+  sp.years = unique(dataset[, c(spID, timeID)])
+  sp.year.count = data.frame(table(sp.years[, spID]))
+  sp.count = merge(sp.site.count, sp.year.count, by = 'Var1', all = T)
+  names(sp.count) = c(spID, 'NumSites', 'NumTimes')
 }
