8c7e77bf6cabcdc97461045e6e503a7f53c11797
Brian S Evans <bsevans.unc@gmail.com>
2014-07-09 11:17:48 -0400

    Rscript for subsetting the Dornelas data to studies that fit our criteria, separating the single dataset into its component datasets, and writing the new datasets to files.

---
+++ scripts/R-scripts/data_extraction_dornelas.R
@@ -0,0 +1,56 @@
+#=========================================================================================
+# SET-UP
+#-----------------------------------------------------------------------------------------
+
+# Libraries:
+
+library(plyr)
+
+# Working directory:
+
+setwd('/home/bsevans/Desktop')
+
+# Get data:
+
+t = read.csv('TSforAllen.csv')
+
+#=========================================================================================
+# SUBSET THE DATA
+#-----------------------------------------------------------------------------------------
+
+# Count the number of years and species for each study:
+
+counts = ddply(t, c('ID'), function(t)c(length(unique(t$Year)), length(unique(t$Species))))
+
+# Subset the counts file to those with >= 5 yrs and >= 10 species:
+
+counts = counts[counts[,2]>4&counts[,3]>9,]
+
+# Subset the Dornelas dataframe to only those files that fit the minimum sample criteria:
+
+t2 = match_df(t,counts, on = 'ID')
+
+# Remove files that can be obtained using Ecodata Retriever:
+
+retrieverIDs = c(18,45,54,56,57,58,59,194)
+
+t = t2[!t2$ID %in% retrieverIDs,]
+
+#=========================================================================================
+# WRITE THE DATA AS SEPARATE DATASETS
+#-----------------------------------------------------------------------------------------
+ 
+# Dataset writer function function:
+
+ds.writer = function(id) {
+  setwd('/home/bsevans/core-transient-datasets')
+  ds = t[t$ID == id,]
+  ds.name = paste('dataset_',id,'.csv',sep='')
+  write.csv(ds, ds.name,row.names = F)
+}
+
+IDs = unique(t$ID)
+for(i in 1:length(IDs)){
+  dswriter(IDs[i])
+}
+
