02733596b727f5f4ffbabe493d266ee72519252a
= <bsevans.unc@gmail.com>
2014-10-01 14:13:01 -0400

    added subfolders to sort working scripts, cleaned datasets, and raw datasets (not yet ready for analysis)

---
+++ data_cleaning_scripts/dwork_228.R
@@ -0,0 +1,46 @@
+# Install and load necessary packages:
+
+install.packages('reshape2')
+library(reshape2)
+
+# Get data:
+
+setwd('/Users/bsevans/Desktop/core-transient-datasets/dataset_228RAW')
+
+hb = read.csv('hb_bird.txt')
+mk = read.csv('mk_bird.txt')
+rp = read.csv('rp_bird.txt')
+sm = read.csv('sm_bird.txt')
+
+reshape.fun = function(sampling.site, site.code){
+  # Remove X's from the year column:
+    names(sampling.site) = gsub('X', '',names(sampling.site))
+  # Melt from wide to long format:
+    dClean = melt(hb, id.vars = 'Bird.Species')
+  # Set column names:
+    names(dClean) = c('species','year','count')
+  # Remove rows of summary data:
+    dClean = dClean[dClean$species!='Total  (all Species)' &
+                  dClean$species!= 'Number of Species',]
+  # Replace "t" (trace?) values with 0's:
+    dClean$count = factor(gsub('t', 0, dClean$count))
+  # There is some r's in there connected with numbers, remove them:
+    dClean$count = gsub('r', '', dClean$count)
+  # Convert count and year data to numeric:
+    dClean$count = as.numeric(dClean$count)
+    dClean$year = as.numeric(levels(dClean$year))[as.integer(dClean$year)]
+  # Add a site column (and arrange as the first column):
+    dClean$site = factor(rep(site.code, length(dClean$year)))
+    dClean = dClean[,c(4,1:3)]
+  # Remove 0's:
+    dClean = dClean[dClean$count>0,]
+    dClean
+   }
+
+d228 = rbind(reshape.fun(hb,'hb'),reshape.fun(mk,'mk'),
+      reshape.fun(rp,'rp'),reshape.fun(sm,'sm'))
+
+setwd('/Users/bsevans/Desktop/core-transient-datasets')
+
+write.csv(d228, 'dataset_228.csv', row.names = F)
+
