dab8e4382a2ba6da3bec376f3864bb6499a9bd4f
= <bsevans.unc@gmail.com>
2014-10-03 15:35:46 -0400

    complete script that formats d232 data

--- data_cleaning_scripts/dwork_232.R
+++ data_cleaning_scripts/dwork_232.R
@@ -21,3 +21,22 @@ d232.1 = ddply(d232, .(location,year, season, night,species),
 
 # Create a data frame of the maximum count of individuals 
 # for a given sampling event within a season.
+
+
+d232.2 = ddply(d232.1,.(location,year,season,species),
+               summarise, count = max(count))
+
+# Combine the year and season columns:
+
+year = as.numeric(paste(d232.2$year,d232.2$season, sep ='.'))
+
+# Arrange the fields in the same order as other datasets:
+
+d232 = data.frame(d232.2[,1],d232.2[,4],year,d232.2[,5])
+names(d232) = c('site','species','year','count')
+
+# Write to file:
+
+write.csv(d232, file.path(out_dir,'dataset_232.csv'))
+
+
