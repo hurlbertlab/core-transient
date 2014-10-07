4512aec6fe3d7fa0581e65cf783b50e9100fa235
bsevansunc <bsevans.unc@gmail.com>
2014-10-07 12:25:09 -0400

    complete data formatting script for dataset_234

--- data_cleaning_scripts/dwork_234.R
+++ data_cleaning_scripts/dwork_234.R
@@ -33,7 +33,29 @@ site = rep('d234_pm',length(d234[,1]))
 
 # Make initial frame (necessary columns, not summarized):
 
-df1 = data.frame(site, d234$species, d234$year)
-  colnames(df1)[2:3] = c('species','year')
+df1 = data.frame(site, d234$species, d234$year, d234$date)
+  colnames(df1)[2:4] = c('species','year','date')
 
--
+# Some species are blank (47 of them) and others are "?" (1) ... remove:
+
+df1 = df1[df1$species!='' & df1$species != '?',]
+
+# Create a data frame of the count of individuals for a given sampling event:
+
+df2 = ddply(df1, .(site, year, species, date), 
+               summarise, count = length(species))
+
+# Create a data frame of the maximum count of individuals 
+# for a given sampling event within a season.
+
+df3 = ddply(df2,.(site,year,species),
+               summarise, count = max(count))
+
+# Arrange the fields in the same order as other datasets:
+
+df4 = data.frame(df3[,1],df3[,3],df3[,2],df3[,4])
+names(df4) = c('site','species','year','count')
+
+# Write to file:
+
+write.csv(df4, file.path(out_dir,'dataset_234.csv'), row.names = F)
