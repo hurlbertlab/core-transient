7500c4d5207236400ee3997ee0fc632ade88ea2d
bsevansunc <bsevans.unc@gmail.com>
2014-10-22 16:40:06 -0400

    added function to summarize across datasets (mean and standard error

--- scripts/R-scripts/core-transient_analysis.R
+++ scripts/R-scripts/core-transient_analysis.R
@@ -117,6 +117,16 @@ for(i in 1:length(sites)){
 
 out.frame = rbind.fill(out.frame)
 
+# Exploring summarizing by dataset
+
+out.by.datasets = ddply(out.frame, .(datasetID), summarize, 
+      mean.prop.core = mean(prop_core), 
+      se.prop.core = sd(prop_core)/sqrt(length(prop_core)),
+      mean.prop.core = mean(prop_trans), 
+      se.prop.core = sd(prop_trans)/sqrt(length(prop_trans)))
+
+out.by.datasets
+
 
 
 
