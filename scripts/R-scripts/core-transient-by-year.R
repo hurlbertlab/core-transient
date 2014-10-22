7728aa4d078493caf0256faa7f01b7a5492e8f54
bsevansunc <bsevans.unc@gmail.com>
2014-10-22 14:34:55 -0400

    separated out example analysis of ct by year into its own analysis

---
+++ scripts/R-scripts/core-transient-by-year.R
@@ -0,0 +1,135 @@
+###################################################################################*
+# ---- SET-UP ----
+###################################################################################*
+
+#----------------------------------------------------------------------------------*
+# ---- Libraries ----
+#==================================================================================*
+
+library(plyr)
+library(ggplot2)
+
+#----------------------------------------------------------------------------------*
+# ---- Get data ----
+#==================================================================================*
+
+# Set read and write directories:
+
+in_dir = 'formatted_datasets'
+
+# Gather all files in directory:
+
+datasets = list.files(in_dir, pattern="*.csv", full.names=T)
+data.list = lapply(datasets, read.csv)
+
+# The following function and line of code will change field names from density
+# to count to ensure equivalent names (for rbinding):
+
+name.changer = function(x){
+  if (names(x)[5] == 'density') names(x)[5] = 'count'
+  x
+}
+
+data.list = lapply(data.list, name.changer)
+
+# Bind the list into a single dataframe that includes all datasets:
+
+d = rbind.fill(data.list)
+
+#----------------------------------------------------------------------------------*
+# ---- Example run, Eastern Wood ----
+#==================================================================================*
+
+d226 = d[d$datasetID == 226,]
+
+# Generate a species list:
+
+sp = unique(d226$species)
+
+# Calculate the number of years sampled:
+
+yrs = length(unique(d226$year))
+
+# Calculate the proportion of years each species was sampled:
+
+prop.yrs = numeric()
+
+for (i in 1:length(sp)){
+  prop.yrs[i] = length(unique(d226[d226$species == sp[i],'year']))/yrs
+}
+
+df1 = data.frame(sp,prop.yrs)
+
+ggplot(df1, aes(x=prop.yrs)) + 
+  geom_histogram(aes(y=..density..), binwidth=.1,
+                 colour="black", fill="gray") + 
+  geom_density(alpha=.2, fill="blue") + 
+  labs(title= 'Proportional densities at Eastern Wood',
+       x = 'Proportion of years', y = 'Density of species/year') +
+  theme(axis.text = element_text(size=14, color = 'black'),
+        axis.title = element_text(size=18),
+        title = element_text(size=22),
+        axis.line = element_line(colour = "black"),
+        panel.background = element_blank())
+
+# Threshold:
+
+core.thresh = 2/3
+trans.thresh = 1/3
+
+# Subset into core and transient:
+
+sp.core = df1[df1$prop.yrs>=core.thresh,]
+
+sp.trans = df1[df1$prop.yrs<=trans.thresh,]
+
+# Richness:
+
+rich.total = length(df1[,1])
+
+rich.core = length(sp.core[,1])
+
+rich.trans = length(sp.trans[,1])
+
+prop.core = rich.core/rich.total
+
+prop.trans = rich.trans/rich.total
+
+#----------------------------------------------------------------------------------*
+# ---- CALCULATE THE CORE-TRANSIENT RICHNESS  BY YEAR, EASTERN WOOD EXAMPLE ----
+#----------------------------------------------------------------------------------*
+
+# Designate species as core or trans
+
+head(df1)
+
+df1$CT = ifelse(df1$prop.yrs>=core.thresh,'core',
+                ifelse(df1$prop.yrs<=trans.thresh,'transient',NA))
+
+# Merge the eastern wood frame with the designation of species
+
+d226a = merge(d226, df1, by.x = 'species',by.y ='sp')
+
+# Calculate the total, core, and transient richness by year
+
+year = sort(unique(d226$year))
+sp.rich = numeric()
+core.rich = numeric()
+trans.rich = numeric()
+for(i in 1:length(year)){
+  sp.rich[i] = length(unique(d226a[d226a$year == year[i],'species']))
+  core.rich[i] = length(unique(d226a[d226a$year == year[i]
+                                     & d226a$CT == 'core','species']))
+  trans.rich[i] = length(unique(d226a[d226a$year == year[i]
+                                      & d226a$CT == 'trans','species']))
+}
+
+# Create a dataframe of the above and calculate the proportional
+# richness for core and transient species for each year:
+
+rich.yr.frame = data.frame(year, sp.rich,core.rich,trans.rich)
+rich.yr.frame$prop.core = core.rich/sp.rich
+rich.yr.frame$prop.trans = trans.rich/sp.rich
+
+ggplot(rich.yr.frame, aes(x = year, y = prop.core)) + geom_point()
+ggplot(rich.yr.frame, aes(x = year, y = prop.trans)) + geom_point()
