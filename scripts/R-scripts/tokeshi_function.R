27319bb4c61ec480f037d02e57a3618243742dc2
bsevansunc <bsevans.unc@gmail.com>
2014-11-05 17:06:25 -0500

    advanced in the function that calculates whether a distribution is uniform, still incomplete

--- scripts/R-scripts/tokeshi_function.R
+++ scripts/R-scripts/tokeshi_function.R
@@ -37,8 +37,18 @@ abline(v = h*N,lty =2)
 # Tokeshi's function to determine if the distribution is different than uniform:
 # Note: Function is currently not finished ... needs to run in a nested for loop (sum of sums)
 
-tokeshi.c.fun = function(N, i, nr, nl, j){
-  (factorial(N)*h^(i + j)*(1-2*h)^(N-i-j))/(factorial(i)*factorial(j)*factorial(S-i-j))
-}
+tokeshi.u.fun = function(N, nr, nl, h){
+  ins.i = nl:N 
+  ins.j = nr:N
+  outs = 0
+  for (i in ins.i){
+    for(j in ins.j){
+      outs = outs + (factorial(N)*h^(ins.i[i] + ins.j[j])*(1-2*h)^(N-ins.i[i]-ins.j[j]))/
+            (factorial(ins.i[i])*factorial(ins.j[j])*factorial(S-ins.i[i]-ins.j[j]))
+      }}
+    outs
+  }
+
+tokeshi.u.fun(N, nr, nl, h)
 
 
