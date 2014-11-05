e74d3b9ee04ce292ac55958356a1368f2ed19751
bsevansunc <bsevans.unc@gmail.com>
2014-11-05 16:00:34 -0500

    added comments

--- scripts/R-scripts/tokeshi_function.R
+++ scripts/R-scripts/tokeshi_function.R
@@ -1,9 +1,15 @@
+# Add date (from the core-transient_analysis script ... avoid plotting if you run the script for the test data)
+
 test = out.raw[[1]]
+
+# Inputs:
 
 N = length(test[,1])               # The total number of species
 nr = length(test[test[,2]>=.66,1]) # The number of species in the upper class
 nl = length(test[test[,2]<=.66,1]) # The number of species in the lower class
 h = .33333333                      # The frequency interval
+
+# Test to determine if there is a greater number of individuals in a bin than random chance:
 
 tokeshi.fun = function(df, N, right.or.left, h){
   outs = numeric()
@@ -16,6 +22,9 @@ tokeshi.fun = function(df, N, right.or.left, h){
 }
 
 tokeshi.fun(test, N, 39, h) # Seems pretty darned conservative!
+
+# Tokeshi's function to determine if the distribution is different than uniform:
+# Note: Function is currently not finished ... needs to run in a nested for loop (sum of sums)
 
 tokeshi.c.fun = function(N, i, nr, nl, j){
   (factorial(N)*h^(i + j)*(1-2*h)^(N-i-j))/(factorial(i)*factorial(j)*factorial(S-i-j))
