57139228957714336bd567aa5eab8b3d493b375f
bsevansunc <bsevans.unc@gmail.com>
2014-11-05 16:21:53 -0500

    fixed the problem in the bin probability ... turns out is was my sequence of numbers!

--- scripts/R-scripts/tokeshi_function.R
+++ scripts/R-scripts/tokeshi_function.R
@@ -6,22 +6,33 @@ test = out.raw[[1]]
 
 N = length(test[,1])               # The total number of species
 nr = length(test[test[,2]>=.66,1]) # The number of species in the upper class
-nl = length(test[test[,2]<=.66,1]) # The number of species in the lower class
+nl = length(test[test[,2]<=.33,1]) # The number of species in the lower class
 h = .33333333                      # The frequency interval
 
 # Test to determine if there is a greater number of individuals in a bin than random chance:
 
 tokeshi.fun = function(df, N, right.or.left, h){
   outs = numeric()
-  ins = seq(right.or.left:N)
+  ins = right.or.left:N
   for(i in 1:length(ins)){
-    outs[i] = factorial(N)/(factorial(ins[i])*factorial(N-ins[i]))*h^ins[i]*(1-h)^(N-ins[i])
+    outs[i] = (factorial(N)/(factorial(ins[i])*factorial(N-ins[i])))*h^ins[i]*(1-h)^(N-ins[i])
   }
   prob = sum(outs)
   return(prob)
 }
 
-tokeshi.fun(test, N, 39, h) # Seems pretty darned conservative!
+tokeshi.fun(test, N,nl, h) 
+
+h = .3333
+x = 1:45
+y = numeric()
+for(i in x){
+  y[i] = tokeshi.fun(test, N, i, h)
+}
+
+plot.new()
+plot(x,y, type = 'l')
+abline(v = h*N,lty =2)
 
 # Tokeshi's function to determine if the distribution is different than uniform:
 # Note: Function is currently not finished ... needs to run in a nested for loop (sum of sums)
