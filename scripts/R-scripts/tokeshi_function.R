7140059ce6da343371a6fbd4e8ba2c76549eb254
bsevansunc <bsevans.unc@gmail.com>
2014-11-06 10:14:32 -0500

    Tokeshi F>f function, fixed plotting and simplified the function

--- scripts/R-scripts/tokeshi_function.R
+++ scripts/R-scripts/tokeshi_function.R
@@ -12,16 +12,19 @@ h = .33333333                      # The frequency interval
 # Test to determine if there is a greater number of individuals in a bin than random chance:
 
 tokeshi.fun = function(df, N, right.or.left, h){
-  outs = numeric()
+  outs = NULL
   ins = right.or.left:N
   for(i in ins){
     outs[i] = (factorial(N)/(factorial(i)*factorial(N-i)))*h^i*(1-h)^(N-i)
   }
-  prob = sum(outs)
-  return(prob)
+  return(sum(na.omit(outs)))
 }
 
 tokeshi.fun(test, N,nl, h) 
+
+# Plotting Tokeshi function to look at # of species per bin:
+# The .05 cut-off signifies a strong mode, .25 a mode, and .5 a weak mode
+# For bimodality both modes must be at least weak:
 
 h = .3333
 x = 1:45
@@ -31,8 +34,13 @@ for(i in x){
 }
 
 plot.new()
-plot(x,y, type = 'l')
-abline(v = h*N,lty =2)
+plot(x,y, xlab = '# of species', ylab = 'P(F>f)', type = 'l', lwd = 2)
+# abline(v = h*N,lty =2)
+abline(h = .05,lty =4)
+abline(h = .25,lty =2)
+abline(h = .5,lty =3)
+
+
 
 # Tokeshi's function to determine if the distribution is different than uniform:
 # Note: Function is currently not finished ... needs to run in a nested for loop (sum of sums)
