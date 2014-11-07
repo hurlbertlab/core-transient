e1ba64d74d13b48e847d7aad515c71030e8ad172
bsevansunc <bsevans.unc@gmail.com>
2014-11-07 16:40:08 -0500

    simplified the histograms dashboard script to remove the function component (now in the ct functions script)

--- scripts/R-scripts/ct_histograms.R
+++ scripts/R-scripts/ct_histograms.R
@@ -11,7 +11,7 @@ prop.df = read.csv('output/prop.df.csv')
 nTime = read.csv('output/Ntime.df.csv')
 outSummary = read.csv('data_source_table.csv')
 
-# Source Tokeshi function for output:
+# Source core-transient functions:
 
 source('scripts/R-scripts/tokeshi_function.R')
 
@@ -19,49 +19,6 @@ library(plyr)
 library(ggplot2)
 library(grid)
 library(gridExtra)
-
-#----------------------------------------------------------------------------------*
-# ---- Function to make core-transient histogram  ----
-#==================================================================================*
-
-ct.hist = function(site,h) {
-  # Get data, subset to a given site:
-    prop.df = prop.df[prop.df$site == site,]
-    outSummary = outSummary[outSummary$dataset_ID == unique(prop.df$dataset),]
-    nTime = nTime[nTime$site == site,]
-  # Get summary stats for subtitle:
-    system = as.character(outSummary$system)
-    taxa = as.character(outSummary$taxa)
-    bimod.parm = round(bimodality(prop.df$occ, nTime$nt),2)
-    mu = round(mean(prop.df$occ))
-    tokeshi = tokeshiFun(site, h)
-  # Plot labels:
-    main = paste('Site ', site, paste('(', system,', ', taxa,')', sep = ''))
-    sub = bquote(b ~ '=' ~ .(bimod.parm) ~ '    '~
-            mu ~ '=' ~ .(mu) ~ '    '~
-            t ~ '=' ~ .(nTime$nt) ~ '    '~
-            P['core'] ~ '=' ~ .(round(tokeshi$Pr,3)) ~ '    '~
-            P['trans'] ~ '=' ~ .(round(tokeshi$Pl,3)))
-  # Set breaks and band width for the histogram:
-    bw = (max(prop.df$occ)-min(prop.df$occ))/10
-    brks = seq(min(prop.df$occ), max(prop.df$occ),bw)
-  # Plot data: 
-    ggplot(prop.df, aes(x=occ)) +
-      geom_histogram(aes(y = ..density..), breaks = brks, right = F,
-                   fill = 'gray', color = 1) +
-      geom_density(alpha=.2, fill="blue") + 
-      # Add labels:
-        xlab('Proportion of temporal samples') + ylab('Density') + 
-        ggtitle(bquote(atop(.(main), atop(.(sub))))) +
-      # Add themes:
-        theme(axis.text = element_text(size=14, color = 1),
-          axis.title.x = element_text(vjust = -1),
-          axis.title.y = element_text(vjust = 2),
-          title = element_text(size=18, vjust = -1),
-          axis.line = element_line(colour = "black"),
-          panel.background = element_blank(),
-          plot.margin = unit(c(.5,.5,1.5,1), "lines"))
-}
 
 #----------------------------------------------------------------------------------*
 # ---- Dashboard: Generate plot output  ----
