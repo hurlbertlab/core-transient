099cdd79d0fbd700cfec8f00a64b48b415769f29
Brian S Evans <bsevans.unc@gmail.com>
2015-01-05 13:07:48 -0500

    conversion to long format, some comment modification

--- scripts/R-scripts/output_plots.R
+++ scripts/R-scripts/output_plots.R
@@ -1,5 +1,6 @@
-# Plot outputs:
+# Libraries:
 
+library(reshape2)
 library(ggplot2)
 library(wesanderson)
 
@@ -12,10 +13,40 @@ ctSummary = read.csv('output/tabular_data/core-transient_summary.csv')
 modeSummary = read.csv('output/tabular_data/ct_mode_summary.csv')
 head(modeSummary)
 
+#----------------------------------------------------------------------------------*
+# ---- Stacked barplot ----
+#==================================================================================*
+
+#----------------------------------------------------------------------------------*
+# Using proportions at a given site
+#----------------------------------------------------------------------------------*
+
+# Make prop data frame with an "other" field representing neither core nor 
+# transient species:
+
 ct.prop = ctSummary[,c(3:4,9:10)]
-ct.prop$prop.nct = 1 - ct$prop.core - ct$prop.trans
+ct.prop$other = 1 - ct$prop.core - ct$prop.trans
+
+# Convert from a wide to long formatted data frame:
+
+ct.prop = melt(ct.prop, id.vars=c('system','taxa'))
+
+names(ct.prop)[3] = 'class'
+
+
+
+
+
+
+#----------------------------------------------------------------------------------*
+# Using richness across sites
+#----------------------------------------------------------------------------------*
 
 ct.rich = ctSummary[,c(3:4,7:8)]
 ct.rich$rich.nct = ct$rich.total - ct$rich.core - ct$rich.trans
+
+
+
+
 
 
