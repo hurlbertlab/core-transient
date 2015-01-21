f81159c2e92973f300e26698fe11981c10c57d24
Michael <auriemma@Bio-HurlbertL01-V220.ad.unc.edu>
2015-01-21 14:31:13 -0500

    made changes to date column and included better notes in script

--- scripts/R-scripts/data_cleaning_scripts/dwork_208_mca.R
+++ scripts/R-scripts/data_cleaning_scripts/dwork_208_mca.R
@@ -22,11 +22,42 @@ head(d1)
 head(d1, 50)
 head(d1, 100)
 head(d1,10)
-
 d = d1
 summary(d)
 
-#==============================================================================
+#================================================================================
+# SITE Column
+
+length(unique(d$treatment))
+unique(d$treatment)
+length(unique(d$replicate))
+unique(d$replicate)
+length(unique(d$station))
+unique(d$station)
+
+# Remove 'treatment' column
+d = d[,-c(2)]
+head(d)
+
+# How many rows in each classification of site
+ddply(d, .(replicate), nrow)
+ddply(d, .(station), nrow)
+
+# Combine replicate and station columns to be specific sites
+site = paste(d$replicate, d$station, sep = '')
+head(site, 50)
+
+# How many records per site now?
+site_table = ddply(data.frame(site), .(site), nrow)
+head(site_table)
+
+# Add site column to dataset and remove old site columns
+d1$site = site
+head(d1)
+d = d1[,-c(2,3,4)]
+head(d)
+
+#============================================================================
 # SPECIES column
 
   #Uppercase all species to remove any capitalization errors
@@ -81,37 +112,40 @@ head(d)
 str(d)
 d$count = as.numeric(d$count1)
 str(d)
-d = d[,-c(7,8)]
+d = d[,-c(6)]
 head(d)
 
 #==============================================================================
 # YEAR column
 
-year = d$sample_record_date
-head(year)
-class(year)
+  # Make date vector into date object using date column from dataset
+date = strptime(d$sample_record_date, '%Y-%m-%d')
+class(date)
+head(date)
 
-  # Make into date object
-year1 = strptime(d$sample_record_date, '%Y-%m-%d')
-class(year1)
-head(year1)
-
-  # Extract year
-year = as.numeric(format(year1,'%Y'))
-head(year)
-tail(year)
-class(year)
-
-  # Add year column to dataset and remove old date column
-d$year = year
+  # Put formatted date column back into dataset
+d$date = date
 head(d)
-tail(d)
-unique(d$sample_record_date)
+str(d)
+
+  #Make date as character for ddply ability
+d$date = as.character(d$date)
+head(d, 50)
+str(d)
+
+  #Remove old date column
 d = d[,-c(1)]
 head(d)
 
-#================================================================================
-# 
+  # Explore number of records taken per day in dataset
+recsperday = ddply(d,.(date), nrow)
+head(recsperday[order(recsperday$V1),], 40)
 
+  # Several sites have small sample of records per day, so look at per month
+  # Remove day from date vector using substring function
+d1 = d
+d1$date = str_sub(d1$date, end = -4)
+head(d1)
 
+  #Check number of records per month
 
