9b348db8a865f8c96906c5960bf8b927277684cf
bsevansunc <bsevans.unc@gmail.com>
2015-01-30 12:04:40 -0500

    added a few comments to make the function easier to understand

--- scripts/R-scripts/beginning_of_new_occPropFun.R
+++ scripts/R-scripts/beginning_of_new_occPropFun.R
@@ -3,12 +3,14 @@ d = read.csv('data/formatted_datasets/dataset_249.csv')
 propOccFun = function(dataset){
   siteNames = unique(dataset$site)
   occPropOutList = list(length = length(siteNames))
+  # For each of the sites in the dataset ...
   for(i in 1:length(siteNames)){
-    # Data subsets for calculate occupancy by site:
+    # Subset to calculate occupancy by site:
       dataSite = subset(d, site == siteNames[i])        
       sp = factor(unique(dataSite$species))
       datasetNum = rep(unique(dataSite$datasetID), length(sp))
-    # For loop to calculate the proportion of years a species has been observed:
+    # For each species ...
+    # Calculate the proportion of years a species has been observed:
       occPropSp = numeric(length = length(sp))
       for (j in 1:length(sp)){
         dataSiteSp = subset(dataSite, species == as.character(sp[j]))
