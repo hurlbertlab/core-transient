1f57edaae1c3a556adc2d305e0c3d431d064f7d7
Michael <auriemma@Bio-HurlbertL01-V220.ad.unc.edu>
2015-02-19 16:23:11 -0500

    saved completed formatting script for 208 and added file to data submod

--- scripts/R-scripts/data_cleaning_scripts/dwork_208_mca.R
+++ scripts/R-scripts/data_cleaning_scripts/dwork_208_mca.R
@@ -1,225 +1,171 @@
-# Formatting dataset 208: Landis long-term insect database
+## Formatting Dataset 238: Plants in Chile
 
-# Get data and open packages
+#-------------------------------------------------------------------------------*
+# ---- SET-UP ----
+#===============================================================================*
 
-getwd()
-setwd('c:/Users/auriemma/core-transient/raw_datasets')
-d = read.csv('dataset_208.csv')
-library(plyr)
+# Load libraries:
+
 library(stringr)
+library(plyr)
 
-# Exploring dataset
+# Source the functions file:
 
-head(d)
-names(d)
-dim(d)
-str(d)
-
-# Remove columns not needed
-
-d1 = d[,-c(1,7,8,10,11)]
-head(d1)
-head(d1, 50)
-head(d1, 100)
-head(d1,10)
-d = d1
-summary(d)
-
-#================================================================================
-# SITE Column
-
-length(unique(d$treatment))
-unique(d$treatment)
-length(unique(d$replicate))
-unique(d$replicate)
-length(unique(d$station))
-unique(d$station)
-
-# Remove 'treatment' column
-d = d[,-c(2)]
-head(d)
-
-# How many rows in each classification of site
-ddply(d, .(replicate), nrow)
-ddply(d, .(station), nrow)
-
-# Combine replicate and station columns to be specific sites
-site = paste(d$replicate, d$station, sep = '')
-head(site, 50)
-
-# How many records per site now?
-site_table = ddply(data.frame(site), .(site), nrow)
-head(site_table)
-
-# Add site column to dataset and remove old site columns
-d1$site = site
-head(d1)
-d = d1[,-c(2,3,4)]
-head(d)
-
-#============================================================================
-# SPECIES column
-
-  #Uppercase all species to remove any capitalization errors
-d$species = toupper(d$species)
-sp = d$species
-unique(sp)
-length(sp)
-  
-  # Removing unwanted species
-remove_sp = c('SOMETHING ELSE',"")
-d1 = d[!d$species %in% remove_sp,]
-unique(d1$species)
-head(d1)
-d = d1
-
-#=============================================================================
-# COUNT column
-
-head(d)
-summary(d)
-str(d$adults)
-  
-  # Create separate count vector
-count = d$adults
-head(count)
-str(count)
-  
-  # Make all values of 0 and blanks 'NA'
-count[count == 0] = NA
-head(count)
-summary(count)
-count[count == ""] = NA
-summary(count)
-
-  # Add column to dataset and check if all data lines up correctly
-d1$count = count
-tail(d1,100)
-d1[500:700,]
-d1[1300:1600,]
-d1[12334:12563,]
-
-  # Remove NA's and 'adults' column
-str(d1)
-d1 = na.omit(d1)
-head(d1)
-d = d[,-c(2)]
-head(d,100)
-
-  # Make count column numeric values
-d$count1 = as.character(d$count)
-head(d)
-str(d)
-d$count = as.numeric(d$count1)
-str(d)
-
-#==============================================================================
-# YEAR column
-
-  # Make date vector into date object using date column from dataset
-date = strptime(d$sample_record_date, '%Y-%m-%d')
-class(date)
-head(date)
-
-  # Put formatted date column back into dataset
-d$date = date
-head(d)
-str(d)
-
-  #Make date as character for ddply ability
-d$date = as.character(d$date)
-head(d, 50)
-str(d)
-
-  #Remove old date column
-d = d[,-c(1)]
-head(d)
-
-  # Explore number of records taken per day in dataset
-recsperday = ddply(d,.(date), nrow)
-head(recsperday[order(recsperday$V1),], 40)
-
-  # Several sites have small sample of records per day, so look at per month
-  # Remove day from date vector using substring function
-d1 = d
-d1$date = str_sub(d1$date, end = -4)
-head(d1)
-
-  #Check number of records per month
-monthly = ddply(d1, .(date), nrow)
-head(monthly[order(monthly$V1),], 10)
-  
-  #Sufficient sample size for each month, lowest is 24 records in one month
-  #Need to convert to decimal years
-  #Separate year and month
-head(date)
-month = str_sub(date, -2)
-head(month)
-year = str_sub(date, end = 4)
-head(year)
-month = as.numeric(month)
-year = as.numeric(year)
-
-  #Decimal year conversion
-month = month/12
-unique(month)
-
-  #Paste together month and year to give decimal year column
-head(d)
-head(d1)
-d1$year_month = year+month
-
-  #Check to see if all lined up
-head(d1)
-summary(d1)
-d1$year_month[12333:12452]
-
-  #switch dataset d1 back to d
-d = d1
-head(d)
-
-  #Remove old date column
-d = d[,-c(5)]
-head(d)
-names(d)[4] = 'year'
-#=================================================================================
-# Finalization of data cleaning, adding of Dataset ID, and checking for error
-  # Add datasetID column
-d$datasetID = rep(208, nrow(d))
-head(d)  
-
-  # Maximum count per site per time period
-d1 = ddply(d, .(datasetID, site, year, species), summarize, count = max(count))
-head(d1)
-dim(d1)
-class(d1$site)  
-  
-  # Change site to factor
-d1$site = factor(d1$site)
-head(d1)
-
-  # Test for each site to have at least 10 different species
-test1 = ddply(d1, .(site), summarize, richness = length(unique(species)))
-head(test1[order(test1$richness),],20)
-
-  # Good, all sites have at least 17 species
-  # Test for sites that have at sufficient number time samples
-test2 = ddply(d1, .(site), summarize, ntime = length(unique(factor(year))))
-head(test2[order(test2$ntime),],20)
-
-  # Passed this test, all sites have at least 92 time samples
-  # No sites removed
-
-head(d1)
-summary(d1)
-
-  # Organize columns
-d = d1[,c(1,2,4,3,5)]
-head(d)
-
-#===========================================================================
-# SAVE and WRITE FILE
-getwd()
 setwd('C:/Users/auriemma/core-transient/')
-list.files()
-write.csv(d, 'formatted_datasets/dataset_208.csv', row.names = F)
+source('scripts/R-scripts/core-transient_functions.R')
+
+# Get data:
+
+getwd()
+
+list.files('data/raw_datasets')
+
+d = read.csv('data/raw_datasets/dataset_208.csv')
+
+#-------------------------------------------------------------------------------*
+# ---- EXPLORE THE DATASET ----
+#===============================================================================*
+head(d)
+str(d)
+names(d)
+
+# Remove unwanted columns
+d1 = d[,-c(2,5,6,8,9)]
+names(d1)
+d = d1
+
+#-------------------------------------------------------------------------------*
+# ---- EXPLORE AND FORMAT SITE DATA ----
+#===============================================================================*
+# Explore
+length(unique(d$Replicate_Station))
+unique(d$Replicate_Station)
+class(d$Replicate_Station)
+
+# Change name
+names(d)[2] = "site"
+head (d)
+unique(d$site)
+
+#-------------------------------------------------------------------------------*
+# ---- EXPLORE AND FORMAT SPECIES DATA ----
+#===============================================================================*
+# Find number unique species
+length(unique(d$Species))
+unique(d$Species)
+
+# Remove unwanted species
+badspp = c('something else')
+d1 = d[!d$Species %in% badspp,]
+dim(d1)
+dim(d)
+unique(d1$Species)
+
+d = d1
+
+# Change name
+names(d)[3] = "species"
+names(d)
+unique(d$species)
+
+#-------------------------------------------------------------------------------*
+# ---- EXPLORE AND FORMAT COUNT DATA ----
+#===============================================================================*
+str(d)
+summary(d)
+unique(d$Adults)
+
+# Change to numeric
+d$Adults = as.character(d$Adults)
+d$Adults = as.numeric(d$Adults)
+str(d)
+
+# Remove NAs
+d = na.omit(d)
+unique(d$Adults)
+length(unique(d$Adults))
+
+# Change name from adults to count
+names(d)[4] = "count"
+head(d)
+unique(d$count)
+
+#-------------------------------------------------------------------------------*
+# ---- EXPLORE AND FORMAT TIME DATA ----
+#===============================================================================*
+# Explore
+length(unique(d$Sample_Date))
+head(d$Sample_Date)
+tail(d$Sample_Date)
+class(d$Sample_Date)
+
+# Change date column to date format
+d$date = strptime(d$Sample_Date, "%Y-%m-%d")
+head(d)
+class(d$date)
+
+# Worked so remove old column
+d = d[,-1]
+head(d)
+
+#-------------------------------------------------------------------------------*
+# ---- MAKE DATA FRAME OF COUNT BY SITES, SPECIES, AND YEAR ----
+#===============================================================================*
+head(d)
+# Add datasetID column
+d$datasetID = rep(208, nrow(d))
+head(d)
+
+# check over dataset
+summary(d)
+
+# Change date back to factor
+d$date = factor(as.character(d$date))
+str(d)
+
+# Make dataframe
+d2 = ddply(d,.(datasetID, site, date, species), summarize, count = max(count))
+
+# Explore dataframe
+head(d2, 30)
+summary(d2)
+
+# Change date back to date object
+d2$date = as.Date(d2$date)
+head(d2, 40)
+summary(d2)
+
+# All good, revert back to d
+d = d2
+
+#-------------------------------------------------------------------------------*
+# ---- WRITE OUTPUT DATA FRAMES  ----
+#===============================================================================*
+head(d)
+class(d$date)
+
+# Write it
+write.csv(d, "data/formatted_datasets/dataset_208.csv", row.names = F)
+
+#-------------------------------------------------------------------------------*
+# ---- EXPLORE YOUR DATASET SUMMARY INFO AND UPDATE THE DATA SOURCE TABLE  ----
+#===============================================================================*
+
+# !!!At this point, go to the data source table and provide:
+#   -central lat and lon (if available, if so, LatLonFLAG = 0, if you couldn't do
+#    it, add a flag of 1)
+#   -spatial_grain columns (T through W)
+#   -nRecs, nSites, nTime, nSpecies
+#   -temporal_grain columns (AH to AK)
+#   -Start and end year
+#   -Any necessary notes
+#   -flag any issues and put issue on github
+#   -git-add-commit-push data_source_table.csv
+
+dim(dataset)
+
+length(unique(dataset$site))
+
+length(unique(dataset$year))
+
+length(unique(dataset$species))
