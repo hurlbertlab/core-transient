e08f968156e3295eaad9491160fe67a109de0b40
Michael <auriemma@Bio-HurlbertL01-V220.ad.unc.edu>
2015-03-02 14:17:08 -0500

    updated script to match template

--- scripts/R-scripts/data_cleaning_scripts/dwork_238_mca.R
+++ scripts/R-scripts/data_cleaning_scripts/dwork_238_mca.R
@@ -20,83 +20,88 @@ getwd()
 
 list.files('data/raw_datasets')
 
-d = read.csv('data/raw_datasets/dataset_238.csv')
+dataset = read.csv('data/raw_datasets/dataset_238.csv')
 
 #-------------------------------------------------------------------------------*
 # ---- EXPLORE THE DATASET ----
 #===============================================================================*
 # Check dimensions, fields, etc
-dim(d)
-names(d)
-summary(d)
+dim(dataset)
+names(dataset)
+summary(dataset)
 
 # Dataset is in wide format organized by date of record (34 different recorded dates)
 # Need to re-format data so is in long form
   # Use 'reshape2' package and melt function
 library('reshape2')
-d.long = melt(d,id.vars = c('record_id','species','trt','grid'))
+d.long = melt(dataset,id.vars = c('record_id','species','trt','grid'))
 head(d.long, 30)
 
 # It worked, but can now remove unwanted columns and call it 'd'
-d = d.long[,-c(1,3)]
-head(d)
+dataset1 = d.long[,-c(1,3)]
+head(dataset1)
 
 # Explore reshaped data structure
-str(d)
+str(dataset1)
+
 # check 'variable' column to see if any dates were lost after reshape
-length(unique(d$variable))
+length(unique(dataset1$variable))
   # no, still 34 dates
 
 # Change names to appropriate 
-names(d) = c('species','site','date','count')
-head(d)
+names(dataset1) = c('species','site','date','count')
+
+# Revert back to dataset
+dataset = dataset1
+head(dataset)
+rm(d.long)
 
 #-------------------------------------------------------------------------------*
 # ---- EXPLORE AND FORMAT SITE DATA ----
 #===============================================================================*
 # Explore unique sites
-class(d$site)
+class(dataset$site)
 
 # Change sites to factor
-d$site = factor(as.character(d$site))
-class(d$site)
-head(d)
-length(unique(d$site))
-unique(d$site)
+dataset$site = factor(as.character(dataset$site))
+class(dataset$site)
+head(dataset)
+length(unique(dataset$site))
+unique(dataset$site)
 
   # 16 different sites labeled 1 thru 16
 
 # See how many records there are per site
-siterecs = table(data.frame(d$site))
+siterecs = table(data.frame(dataset$site))
 siterecs
   # Same amount of records for each, 1496 records per site
 # check random spot in data for format
-d[300:350,]
+dataset[300:350,]
 # Site data appears all good
 
 #-------------------------------------------------------------------------------*
 # ---- EXPLORE AND FORMAT SPECIES DATA ----
 #===============================================================================*
 # Explore species
-length(unique(d$species))
+length(unique(dataset$species))
   # 44 Unique coded species
-unique(d$species)
+unique(dataset$species)
 
 # Remove unwanted species
 badsp = c('OTHERS')
-d1 = d[!d$species %in% badsp,]
-unique(d1$species)
-length(unique(d1$species))
+dataset1 = dataset[!dataset$species %in% badsp,]
+unique(dataset1$species)
+length(unique(dataset1$species))
   # Worked, now only 43 unique species
 
 # How many lost rows
-nrow(d)
-nrow(d1)
+nrow(dataset)
+nrow(dataset1)
 
-d = d1
+dataset = dataset1
 
 # Explored the coding of species in this study and found they
-# are lised as just 4 letter abbreviations for speciefic species 
+# are lised as just 4 letter abbreviations for specific species 
 # within the experimental plots.  All species shown here were 
 # confirmed and all bad spp were removed.
 
@@ -104,17 +109,17 @@ d = d1
 # ---- EXPLORE AND FORMAT TIME DATA ----
 #===============================================================================*
 # Explore dates and format
-class(d$date)
-levels(d$date)
+class(dataset$date)
+levels(dataset$date)
 
 # Separate month and year
-month = str_sub(d$date, end = 3)
+month = str_sub(dataset$date, end = 3)
 unique(month)
   #change to factor
 month = factor(month)
 levels(month)
 
-year = str_sub(d$date, start = -2, end = -1)
+year = str_sub(dataset$date, start = -2, end = -1)
 class(year)
 unique(year)
   # change to factor
@@ -132,15 +137,14 @@ levels(year) = c("2002", "1989", "1990", "1991", "1992", "1993", "1994", "1995",
 head(year)
 tail(year)
 
-
 # Add date to dataset
-d1$date = paste(month, year, sep = '/')
-head(d1,20)
-tail(d1, 20)
+dataset1$date = paste(month, year, sep = '/')
+head(dataset1,20)
+tail(dataset1, 20)
 
 # change date to date format
   # Make dates as.character first
-date = factor(d1$date)
+date = factor(dataset1$date)
 head(date)
 class(date)
 date2 = strptime(date, format = "%m/%Y")
@@ -149,12 +153,12 @@ head(date2)
 tail(date2)
 # !!!This strptime DID NOT WORK without day.(returned all NAs) 
 # so need to add first of month (vector of 1s) to do %d/%m/%Y format
-day = rep(1, nrow(d1))
-d1$date = paste(day,month,year,sep = "/")
-head(d1)
+day = rep(1, nrow(dataset1))
+dataset1$date = paste(day,month,year,sep = "/")
+head(dataset1)
 
 # change to date format
-date = as.character(d1$date)
+date = as.character(dataset1$date)
 date1 = strptime(date, "%d/%m/%Y")
 head(date1)
 tail(date1)
@@ -162,71 +166,71 @@ class(date1)
 # This worked, so made note in data source table
 
 # Replace with this new date column
-head(d1)
-d1$date = date1
-head(d1)
-str(d1)
-summary(d1)
-unique(d1$date)
+head(dataset1)
+dataset1$date = date1
+head(dataset1)
+str(dataset1)
+summary(dataset1)
+unique(dataset1$date)
 
 # Revert back to d
-d = d1
-head(d, 20)
+dataset = dataset1
+head(dataset, 20)
 
 #-------------------------------------------------------------------------------*
 # ---- EXPLORE AND FORMAT COUNT DATA ----
 #===============================================================================*
 # Explore data
-summary(d$count)
-summary(d)
-length(unique(d$count))
-unique(d$count)
+summary(dataset$count)
+summary(dataset)
+length(unique(dataset$count))
+unique(dataset$count)
 
 # Change to numeric
-d$count = as.numeric(d$count)
-length(unique(d$count))
-unique(d$count)
+dataset$count = as.numeric(dataset$count)
+length(unique(dataset$count))
+unique(dataset$count)
   # there are NAs and zeros
 
 # Remove NAs and zeros
-d = d[d$count>0,]
-length(unique(d$count))
-head(d)
-dim(d1)
-dim(d)
+dataset = dataset[dataset$count>0,]
+length(unique(dataset$count))
+head(dataset)
+dim(dataset1)
+dim(dataset)
 
-d = na.omit(d)
-dim(d)
-length(unique(d$count))
+dataset = na.omit(dataset)
+dim(dataset)
+length(unique(dataset$count))
 
 # Check 
-summary(d)
+summary(dataset)
 
 #-------------------------------------------------------------------------------*
 # ---- MAKE DATA FRAME OF COUNT BY SITES, SPECIES, AND YEAR ----
 #===============================================================================*
 # Add dataset ID column
-d$datasetID = rep(238, nrow(d))
-head(d,10)
+dataset$datasetID = rep(238, nrow(dataset))
+head(dataset,10)
 
 # Make dataframe
-str(d)
+str(dataset)
   # Change date to factor
-d$date = factor(as.character(d$date))
-head(d)
-d1 = ddply(d, .(datasetID, site, date, species), summarize, count = max(count))
+dataset$date = factor(as.character(dataset$date))
+head(dataset)
+dataset1 = ddply(dataset, .(datasetID, site, date, species), summarize, count = max(count))
 
 # Check the dataframe
-summary(d1)
-head(d1, 30)
+summary(dataset1)
+head(dataset1, 30)
 
 # Convert date back to date object
-d1$date = as.Date(d1$date)
-head(d1)
-summary(d1)
+dataset1$date = as.Date(dataset1$date)
+head(dataset1)
+summary(dataset1)
 
 # All looks good
-d = d1
+dataset = dataset1
 #-------------------------------------------------------------------------------*
 # ---- WRITE OUTPUT DATA FRAMES  ----
 #===============================================================================*
