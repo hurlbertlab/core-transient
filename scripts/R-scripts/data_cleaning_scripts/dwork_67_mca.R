73742156c420f30bd3c8c7b4f35cb2bd18c12b4d
unknown <auriemma@Bio-HurlbertL06-7479.ad.unc.edu>
2015-03-24 15:58:17 -0400

    created file for formatting 67

---
+++ scripts/R-scripts/data_cleaning_scripts/dwork_67_mca.R
@@ -0,0 +1,58 @@
+# Formatting Dataset 67: OBIS Birds
+
+#-------------------------------------------------------------------------------*
+# ---- SET-UP ----
+#===============================================================================*
+
+# Load libraries:
+
+library(stringr)
+library(plyr)
+library(ggplot2)
+library(grid)
+library(gridExtra)
+library(MASS)
+
+# Source the functions file:
+
+getwd()
+
+source('scripts/R-scripts/core-transient_functions.R')
+
+# Get data. First specify the dataset number ('ds') you are working with.
+
+ds = 67 
+
+list.files('data/raw_datasets')
+
+dataset = read.csv(paste('data/raw_datasets/dataset_', ds, '.csv', sep = ''))
+
+dataFormattingTable = read.csv('Reference/data_formatting_table.csv')
+
+#-------------------------------------------------------------------------------*
+# ---- EXPLORE THE DATASET ----
+#===============================================================================*
+names(dataset)
+str(dataset)
+head(dataset)
+
+# Remove unused field labeled 'X'
+dataset1 = dataset[,-1]
+head(dataset1)
+
+# Change SampleID field to 'site'
+names(dataset1)[3] = 'site'
+
+#check 
+
+head(dataset1)
+
+# !GIT-ADD-COMMIT-PUSH AND DESCRIBE HOW THE DATA WERE MODIFIED!
+
+#!DATA FORMATTING TABLE UPDATE! 
+# Are the ONLY site identifiers the latitude and longitude of the observation or 
+# sample? (I.e., there are no site names or site IDs or other designations) Y/N
+
+dataFormattingTable[,'LatLong_sites'] = 
+  dataFormattingTableFieldUpdate(ds, 'LatLong_sites', 'N') 
+
