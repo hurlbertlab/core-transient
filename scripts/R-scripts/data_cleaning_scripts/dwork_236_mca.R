3baadaf54c535a72b9e950df75c0275c87c887cb
unknown <auriemma@Bio-HurlbertL06-7479.ad.unc.edu>
2015-03-26 15:59:09 -0400

    made compiled dataframe

--- scripts/R-scripts/data_cleaning_scripts/dwork_236_mca.R
+++ scripts/R-scripts/data_cleaning_scripts/dwork_236_mca.R
@@ -247,3 +247,24 @@ dataFormattingTable[,'Notes_timeFormat'] =
 dataFormattingTable[,'subannualTgrain'] = 
   dataFormattingTableFieldUpdate(ds, 'subannualTgrain',  'Y')
 
+#-------------------------------------------------------------------------------*
+# ---- MAKE DATA FRAME OF COUNT BY SITES, SPECIES, AND YEAR ----
+#===============================================================================*
+#add the datasetID:
+
+dataset6$datasetID = ds
+
+# Now make the compiled dataframe:
+
+dataset7 = ddply(dataset6,.(datasetID, site, date, species),
+                 summarize, count = max(count))
+
+# Explore the data frame:
+
+dim(dataset7)
+
+head(dataset7)
+
+summary(dataset7)
+
+# !GIT-ADD-COMMIT-PUSH AND DESCRIBE HOW THE DATA WERE MODIFIED!
