d0372c5ea53f0e09edc6b493c1414fc956cf001b
unknown <auriemma@Bio-HurlbertL06-7479.ad.unc.edu>
2015-03-30 13:26:48 -0400

    removed column and changed field names

---
+++ scripts/R-scripts/data_cleaning_scripts/dwork_147_mca.R
@@ -0,0 +1,64 @@
+# Formatting Dataset 147 OBIS Invert
+
+#-------------------------------------------------------------------------------*
+# ---- SET-UP ----
+#===============================================================================*
+
+# Load libraries:
+
+library(stringr)
+library(plyr)
+library(ggplot2)
+library(grid)
+library(gridExtra)
+library(MASS)
+
+
+# Source the functions file:
+
+getwd()
+
+source('scripts/R-scripts/core-transient_functions.R')
+
+# Get data. First specify the dataset number ('ds') you are working with.
+
+ds = 147
+
+list.files('data/raw_datasets')
+
+dataset = read.csv(paste('data/raw_datasets/dataset_', ds, '.csv', sep = ''))
+
+dataFormattingTable = read.csv('Reference/data_formatting_table.csv')
+
+#-------------------------------------------------------------------------------*
+# ---- EXPLORE THE DATASET ----
+#===============================================================================*
+
+head(dataset, 10)
+unique(dataset$SampleID)
+
+# Remove column called 'X'
+
+dataset1 = dataset[,-1]
+head(dataset1)
+
+# Change ID column to 'datasetID'
+
+names(dataset1)[1] = 'datasetID'
+
+# Change name of site column
+
+names(dataset1)[3] = 'site'
+
+head(dataset1)
+
+# !GIT-ADD-COMMIT-PUSH AND DESCRIBE HOW THE DATA WERE MODIFIED!
+
+#!DATA FORMATTING TABLE UPDATE! 
+# Are the ONLY site identifiers the latitude and longitude of the observation or 
+# sample? (I.e., there are no site names or site IDs or other designations) Y/N
+
+dataFormattingTable[,'LatLong_sites'] = 
+  dataFormattingTableFieldUpdate(ds, 'LatLong_sites','N') 
+
+
