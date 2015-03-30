6cd599e2168ced83bb5c7450d9fe1bcd349d9141
unknown <auriemma@Bio-HurlbertL06-7479.ad.unc.edu>
2015-03-30 10:42:12 -0400

    removed unwanted columns git commit -m "removed unwanted columns" .. `

---
+++ scripts/R-scripts/data_cleaning_scripts/dwork_124_mca.R
@@ -0,0 +1,61 @@
+# Formatting Dataset 124: OBIS Marine Inverts
+
+#-------------------------------------------------------------------------------*
+# ---- SET-UP ----
+#===============================================================================*
+
+# Load libraries:
+
+library(stringr)
+library(plyr)
+library(ggplot2)
+library(grid)
+library(gridExtra)
+library(MASS)
+
+
+# Source the functions file:
+
+getwd()
+setwd('C:/Users/auriemma//core-transient')
+source('scripts/R-scripts/core-transient_functions.R')
+
+# Get data. First specify the dataset number ('ds') you are working with.
+
+ds = 124 
+
+list.files('data/raw_datasets')
+
+dataset = read.csv(paste('data/raw_datasets/dataset_', ds, '.csv', sep = ''))
+
+dataFormattingTable = read.csv('Reference/data_formatting_table.csv')
+
+#-------------------------------------------------------------------------------*
+# ---- EXPLORE THE DATASET ----
+#===============================================================================*
+
+head(dataset)
+str(dataset)
+summary(dataset)
+tail(dataset)
+
+# Remove column called 'X'
+dataset1 = dataset[,-1]
+head(dataset1)
+
+# Change ID column to 'datasetID'
+names(dataset1)[1] = 'datasetID'
+
+# Change name of site column
+names(dataset1)[3] = 'site'
+
+head(dataset1)
+
+# !GIT-ADD-COMMIT-PUSH AND DESCRIBE HOW THE DATA WERE MODIFIED!
+
+#!DATA FORMATTING TABLE UPDATE! 
+# Are the ONLY site identifiers the latitude and longitude of the observation or 
+# sample? (I.e., there are no site names or site IDs or other designations) Y/N
+
+dataFormattingTable[,'LatLong_sites'] = 
+  dataFormattingTableFieldUpdate(ds, 'LatLong_sites','Y') 
