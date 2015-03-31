dbd4d41b089a65a812f446c1ba00857b14d30324
bsevansunc <bsevans.unc@gmail.com>
2015-03-31 16:57:50 -0400

    added getMaxWz function

--- scripts/R-scripts/propOcc_scripts/evaluate_test_datasets.R
+++ scripts/R-scripts/propOcc_scripts/evaluate_test_datasets.R
@@ -1,25 +1,22 @@
 test = fakeData(2,2,10,2)
 
-test1a = fakeData(2,2,10,2)
-
-
 nestedDataset = getNestedSiteDataset(test)
 
 wzMaker2 = function(i){
   
   siteGrain = nestedDataset[[2]][i]
-  nestedDataset = nestedDataset[[1]]
-  nestedDataset$siteGrain = nestedDataset[,siteGrain]
+  nestedDatasetDf = nestedDataset[[1]]
+  nestedDatasetDf$siteGrain = nestedDatasetDf[,siteGrain]
   
   # Subset to sites with a high enough species richness and year samples:
   
-  siteSr_nTime = ddply(nestedDataset, .(siteGrain), summarize,
+  siteSr_nTime = ddply(nestedDatasetDf, .(siteGrain), summarize,
                        sr = length(unique(species)), 
                        nTime = length(unique(year)))
   
   goodSites = subset(siteSr_nTime, sr >= 10 & siteSr_nTime$nTime >= 5)$siteGrain
   
-  d1 = nestedDataset[nestedDataset$siteGrain %in% goodSites,]
+  d1 = nestedDatasetDf[nestedDatasetDf$siteGrain %in% goodSites,]
   
   # Get data frame of the number of spatial and temporal samples by site and year:
   
@@ -44,16 +41,19 @@ wzMaker2 = function(i){
       siteSub = subset(spaceTime, siteGrain  == sites[i])
       w = wz[j, 1]
       z = wz[j, 2]
+      YearsGTEwz = sum(siteSub$spatialSubsamples >= w &
+                         siteSub$temporalSubsamples >= z)
+      GTEYearThreshold = ifelse(YearsGTEwz >= 5, 1, 0)
       outList[[i]] = data.frame(site = unique(siteSub$siteGrain), 
-                                w = w, z = z, 
-                                YearsGTEwz = sum(siteSub$spatialSubsamples >= w &
-                                    siteSub$temporalSubsamples >= z))
+                                w , z, 
+                                YearsGTEwz,
+                                GTEYearThreshold)
     }}
   
   # This dataframe provides the calculation of the number of years a site was greater than or equal to w and z for each site and potential value of w and z:
   
   wzFrame = rbind.fill(outList)
-
+  
   # Output:
   
   outList = list(spatialGrain, wz, wzFrame, spaceTime)
@@ -65,6 +65,29 @@ wzMaker2 = function(i){
 wzMaker2(2)
 
 wzMaker2(1)
+
+getMaxWz = function(i){
+  wzFrame = wzMaker2(i)[['wzFrame']]
+  wzList = list(length = nrow(wzFrame))
+  nSites = nrow(wzFrame)
+  wzCombinationSum = ddply(wzFrame, .(w, z), summarize, 
+                           wzSum = sum(GTEYearThreshold))
+  
+  wzCombinationSum$siteProp = wzCombinationSum$wzSum/nSites  
+  wzFrameGTE20 = subset(wzCombinationSum, siteProp >= .2) 
+  wzFrameGTEmax = subset(wzFrameGTE20, w + z == max(w+z))
+  return(wzFrameGTEmax)
+}
+
+wzMaker2(1)[[3]]
+
+getMaxWz(1)
+
+wzMaker2(2)[[3]]
+
+getMaxWz(2)
+
+
 
 
 
