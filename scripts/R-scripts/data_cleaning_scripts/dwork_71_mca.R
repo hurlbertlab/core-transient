d02f9638ab64d53ed166877f62b1ae77b0af1c46
unknown <auriemma@Bio-HurlbertL06-7479.ad.unc.edu>
2015-04-01 13:55:37 -0400

    made file for 71 and removed 1 column from dataset

---
+++ scripts/R-scripts/data_cleaning_scripts/dwork_71_mca.R
@@ -0,0 +1,51 @@
+# Formatting Dataset 71: OBIS ArcOD Plankton
+
+#-------------------------------------------------------------------------------*
+# ---- SET-UP ----
+#===============================================================================*
+
+# Load libraries:
+
+library(stringr)
+library(plyr)
+library(ggplot2)
+library(grid)
+library(gridExtra)
+library(MASS)
+
+
+# Source the functions file:
+
+getwd()
+
+source('scripts/R-scripts/core-transient_functions.R')
+
+# Get data. First specify the dataset number ('ds') you are working with.
+
+ds = 71
+
+list.files('data/raw_datasets')
+
+dataset = read.csv(paste('data/raw_datasets/dataset_', ds, '.csv', sep = ''))
+
+dataFormattingTable = read.csv('Reference/data_formatting_table.csv')
+
+#-------------------------------------------------------------------------------*
+# ---- EXPLORE THE DATASET ----
+#===============================================================================*
+
+names(dataset)
+head(dataset)
+str(dataset)
+summary(dataset)
+
+# Remove column 'X'
+
+dataset1 = dataset[,-1]
+head(dataset1)
+
+# Rename SampleID as site, and ID as datasetID
+
+names(dataset1)[1] = 'datasetID'
+names(dataset1)[3] = 'site'
+head(dataset1)
