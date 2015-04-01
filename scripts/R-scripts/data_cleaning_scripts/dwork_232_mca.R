394eb0691d2f22afc190a67d34f30b557a1d3ab6
unknown <auriemma@Bio-HurlbertL06-7479.ad.unc.edu>
2015-04-01 12:13:13 -0400

    concatenated site data and editted columns

--- scripts/R-scripts/data_cleaning_scripts/dwork_232_mca.R
+++ scripts/R-scripts/data_cleaning_scripts/dwork_232_mca.R
@@ -62,3 +62,46 @@ summary(dataset1)
 
 dataFormattingTable[,'LatLong_sites'] = 
   dataFormattingTableFieldUpdate(ds, 'LatLong_sites','N') 
+
+#-------------------------------------------------------------------------------*
+# ---- EXPLORE AND FORMAT SITE DATA ----
+#===============================================================================*
+
+# Sites are nested in this dataset.  Sites are listed, where there are separate webs and individual traps within the webs
+
+site = paste(dataset1$location, dataset1$web, dataset1$trap, sep = "_")
+
+# comparing the site fields in the dataset with the new vector of sites:
+
+head(site)
+
+# Looks good, now add site to dataset and remove old site fields
+
+dataset2 = dataset1
+
+dataset2$site = factor(site)
+
+dataset2 = dataset2[,-c(2,5,6)]
+head(dataset2)
+
+# All good
+
+# !GIT-ADD-COMMIT-PUSH AND DESCRIBE HOW THE SITE DATA WERE MODIFIED!
+
+# !DATA FORMATTING TABLE UPDATE! 
+
+# Raw_siteUnit. 
+
+dataFormattingTable[,'Raw_siteUnit'] = 
+  dataFormattingTableFieldUpdate(ds, 'Raw_siteUnit', 'location_web_trap') 
+
+# spatial_scale_variable. Is a site potentially nested (e.g., plot within a quad or decimal lat longs that could be scaled up)? Y/N
+
+dataFormattingTable[,'spatial_scale_variable'] = 
+  dataFormattingTableFieldUpdate(ds, 'spatial_scale_variable','Y')
+
+# Notes_siteFormat. Use this field to THOROUGHLY describe any changes made to the site field during formatting.
+
+dataFormattingTable[,'Notes_siteFormat'] = 
+  dataFormattingTableFieldUpdate(ds, 'Notes_siteFormat', 'site fields concatenated. after looking through metadata descriptions, it seems that location_web_trap describes the order of nested sites from small to large.')
+
