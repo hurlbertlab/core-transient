937577abaaafe51b0cd56a76b7622365dc7febcc
Michael <auriemma@Bio-HurlbertL01-V220.ad.unc.edu>
2015-04-06 11:12:05 -0400

    no changes made to sites

--- scripts/R-scripts/data_cleaning_scripts/dwork_71_mca.R
+++ scripts/R-scripts/data_cleaning_scripts/dwork_71_mca.R
@@ -24,8 +24,6 @@ source('scripts/R-scripts/core-transient_functions.R')
 
 ds = 71
 
-list.files('data/raw_datasets')
-
 dataset = read.csv(paste('data/raw_datasets/dataset_', ds, '.csv', sep = ''))
 
 dataFormattingTable = read.csv('Reference/data_formatting_table.csv')
@@ -71,4 +69,40 @@ unique(dataset1$site)
 
 # Site names have A LOT of unnecessary information like species names, sampling methods, etc
 
+# No Lat_longs, just region and possibly a region or site number
+
 dataset2 = dataset1
+
+dataset2$site = factor(dataset2$site)
+
+str(dataset2)
+
+# For now, leaving sites as they are until further decision is made. 
+
+# !GIT-ADD-COMMIT-PUSH AND DESCRIBE HOW THE SITE DATA WERE MODIFIED!
+
+# !DATA FORMATTING TABLE UPDATE! 
+
+# Raw_siteUnit. How a site is coded 
+
+dataFormattingTable[,'Raw_siteUnit'] = 
+  dataFormattingTableFieldUpdate(ds, 'Raw_siteUnit', 'region_site_sitenumber_') 
+
+
+# spatial_scale_variable. Is a site potentially nested (e.g., plot within a quad or decimal lat longs that could be scaled up)? Y/N
+
+dataFormattingTable[,'spatial_scale_variable'] = 
+  dataFormattingTableFieldUpdate(ds, 'spatial_scale_variable', 'N') 
+
+# Notes_siteFormat. Use this field to THOROUGHLY describe any changes made to the site field during formatting.
+
+dataFormattingTable[,'Notes_siteFormat'] = 
+  dataFormattingTableFieldUpdate(ds, 'Notes_siteFormat', 'Sites have a lot of unnecessary information including species names, catch methods, etc. No changes made to site data, need to find way to remove some unwanted info from sites')
+
+#-------------------------------------------------------------------------------*
+# ---- EXPLORE AND FORMAT SPECIES DATA ----
+#===============================================================================*
+
+# Look at the individual species present:
+
+levels(dataset2$species) 
