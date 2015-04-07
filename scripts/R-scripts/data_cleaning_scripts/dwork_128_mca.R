9ec6274ea122704c4b3210efebdb67b806f82964
unknown <auriemma@Bio-HurlbertL06-7479.ad.unc.edu>
2015-04-07 14:31:05 -0400

    no sites removed

--- scripts/R-scripts/data_cleaning_scripts/dwork_128_mca.R
+++ scripts/R-scripts/data_cleaning_scripts/dwork_128_mca.R
@@ -62,3 +62,38 @@ head(dataset1)
 dataFormattingTable[,'LatLong_sites'] = 
   dataFormattingTableFieldUpdate(ds, 'LatLong_sites', 'N') 
 
+#-------------------------------------------------------------------------------*
+# ---- EXPLORE AND FORMAT SITE DATA ----
+#===============================================================================*
+
+# explore
+
+length(unique(dataset1$site))
+
+levels(dataset1$site)
+
+# Sites are listed by USA_Atlantic_sitenumber
+# After looking through all sites, seem all good, so no changes to be made
+
+dataset2 = dataset1
+
+# !GIT-ADD-COMMIT-PUSH AND DESCRIBE HOW THE SITE DATA WERE MODIFIED!
+
+# !DATA FORMATTING TABLE UPDATE! 
+
+# Raw_siteUnit. How a site is coded (i.e. if the field was concatenated such as this one, it was coded as "site_block_treatment_plot_quad"). Alternatively, if the site were concatenated from latitude and longitude fields, the encoding would be "lat_long". 
+
+dataFormattingTable[,'Raw_siteUnit'] = 
+  dataFormattingTableFieldUpdate(ds, 'Raw_siteUnit', 'Ocean_sitenumber') 
+
+
+# spatial_scale_variable. Is a site potentially nested (e.g., plot within a quad or decimal lat longs that could be scaled up)? Y/N
+
+dataFormattingTable[,'spatial_scale_variable'] = 
+  dataFormattingTableFieldUpdate(ds, 'spatial_scale_variable','N')
+
+# Notes_siteFormat. Use this field to THOROUGHLY describe any changes made to the site field during formatting.
+
+dataFormattingTable[,'Notes_siteFormat'] = 
+  dataFormattingTableFieldUpdate(ds, 'Notes_siteFormat', 'No changes made.  sites are listed as USA_Atlantic_sitenumber.  site numbers go by intervals of 0.5, no metadata found to determine what these numbers represent.')
+
