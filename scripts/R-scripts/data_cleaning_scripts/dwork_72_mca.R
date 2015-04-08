cb0ce5dea33429f9306ace21132b12c096a69abc
unknown <auriemma@Bio-HurlbertL06-7479.ad.unc.edu>
2015-04-08 13:27:49 -0400

    removed column X, no sites removed or changed

---
+++ scripts/R-scripts/data_cleaning_scripts/dwork_72_mca.R
@@ -0,0 +1,93 @@
+# Formatting Dataset 71: OBIS ArcOD Zooplankton
+
+#-------------------------------------------------------------------------------*
+# ---- SET-UP ----
+#===============================================================================*
+
+# Load libraries:
+
+library(stringr)
+library(plyr)
+library(ggplot2)
+library(grid)
+library(gridExtra)
+library(MASS)
+
+# Source the functions file:
+
+getwd()
+
+source('scripts/R-scripts/core-transient_functions.R')
+
+# Get data. First specify the dataset number ('ds') you are working with.
+
+ds = 72
+
+dataset = read.csv(paste('data/raw_datasets/dataset_', ds, '.csv', sep = ''))
+
+dataFormattingTable = read.csv('Reference/data_formatting_table.csv')
+
+#-------------------------------------------------------------------------------*
+# ---- EXPLORE THE DATASET ----
+#===============================================================================*
+
+names(dataset)
+head(dataset)
+str(dataset)
+summary(dataset)
+
+# Remove column 'X'
+
+dataset1 = dataset[,-1]
+head(dataset1)
+
+# Rename SampleID as site, and ID as datasetID
+
+names(dataset1)[1] = 'datasetID'
+names(dataset1)[3] = 'site'
+head(dataset1, 30)
+
+# !GIT-ADD-COMMIT-PUSH AND DESCRIBE HOW THE DATA WERE MODIFIED!
+
+#!DATA FORMATTING TABLE UPDATE! 
+# Are the ONLY site identifiers the latitude and longitude of the observation or 
+# sample? (I.e., there are no site names or site IDs or other designations) Y/N
+
+dataFormattingTable[,'LatLong_sites'] = 
+  dataFormattingTableFieldUpdate(ds, 'LatLong_sites', 'N')
+
+#-------------------------------------------------------------------------------*
+# ---- EXPLORE AND FORMAT SITE DATA ----
+#===============================================================================*
+
+# Explore
+
+length(unique(dataset1$site))
+summary(dataset1)
+levels(dataset1$site)
+
+# Sites have a lot of information in them; seems like some mesh size, some species or catch info, and several number combinations. No lat_longs. Cannot find OBIS metadata info, so keep all these sites
+
+dataset2 = dataset1
+
+# For now, leaving sites as they are until further decision is made. 
+
+# !GIT-ADD-COMMIT-PUSH AND DESCRIBE HOW THE SITE DATA WERE MODIFIED!
+
+# !DATA FORMATTING TABLE UPDATE! 
+
+# Raw_siteUnit. How a site is coded 
+
+dataFormattingTable[,'Raw_siteUnit'] = 
+  dataFormattingTableFieldUpdate(ds, 'Raw_siteUnit', 'sites coded') 
+
+# spatial_scale_variable. Is a site potentially nested (e.g., plot within a quad or decimal lat longs that could be scaled up)? Y/N
+
+dataFormattingTable[,'spatial_scale_variable'] = 
+  dataFormattingTableFieldUpdate(ds, 'spatial_scale_variable', 'N') 
+
+# Notes_siteFormat. Use this field to THOROUGHLY describe any changes made to the site field during formatting.
+
+dataFormattingTable[,'Notes_siteFormat'] = 
+  dataFormattingTableFieldUpdate(ds, 'Notes_siteFormat', 'Sites have a lot of  information including net mesh size, some species or catch information, and several number combinations. No lat_longs. No changes made to site data, need to find metdata for info on site coding')
+
