2019f86b66fad680c544906a98ca1ef61807f9c5
unknown <auriemma@Bio-HurlbertL06-7479.ad.unc.edu>
2015-04-09 16:08:36 -0400

    editted site data

--- scripts/R-scripts/data_cleaning_scripts/dwork_85_mca.R
+++ scripts/R-scripts/data_cleaning_scripts/dwork_85_mca.R
@@ -56,3 +56,50 @@ tail(dataset1)
 dataFormattingTable[,'LatLong_sites'] = 
   dataFormattingTableFieldUpdate(ds, 'LatLong_sites', 'Y')
 
+#-------------------------------------------------------------------------------*
+# ---- EXPLORE AND FORMAT SITE DATA ----
+#===============================================================================*
+
+# Explore
+
+length(unique(dataset1$site))
+summary(dataset1)
+
+# View all sites
+
+levels(dataset1$site)
+
+# Sites are all listed as 'Netherlands_site#_site#'
+
+# Can substring out Netherlands section
+
+dataset2 = dataset1
+
+dataset2$site = str_sub(dataset2$site, start=14)
+
+# check
+
+head(dataset2)
+tail(dataset2)
+
+#  just site numbers left in data
+
+# !GIT-ADD-COMMIT-PUSH AND DESCRIBE HOW THE SITE DATA WERE MODIFIED!
+
+# !DATA FORMATTING TABLE UPDATE! 
+
+# Raw_siteUnit. How a site is coded 
+
+dataFormattingTable[,'Raw_siteUnit'] = 
+  dataFormattingTableFieldUpdate(ds, 'Raw_siteUnit', 'Netherlands_sitenumber_sitenumber') 
+
+# spatial_scale_variable. Is a site potentially nested (e.g., plot within a quad or decimal lat longs that could be scaled up)? Y/N
+
+dataFormattingTable[,'spatial_scale_variable'] = 
+  dataFormattingTableFieldUpdate(ds, 'spatial_scale_variable', 'N') 
+
+# Notes_siteFormat.
+
+dataFormattingTable[,'Notes_siteFormat'] = 
+  dataFormattingTableFieldUpdate(ds, 'Notes_siteFormat', 'Sites were all written as Netherlands_sitenumber_sitenumber.  Used substring to cut out Netherlands, leaving only site numbers in the data.  No sites removed.")
+
