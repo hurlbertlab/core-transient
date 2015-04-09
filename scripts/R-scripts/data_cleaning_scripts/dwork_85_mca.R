88ae7a51bff35d2dcd5242b54591e38c61b48698
unknown <auriemma@Bio-HurlbertL06-7479.ad.unc.edu>
2015-04-09 15:57:31 -0400

    removed column X and changed 2 column names

---
+++ scripts/R-scripts/data_cleaning_scripts/dwork_85_mca.R
@@ -0,0 +1,58 @@
+# Formatting Dataset 84: EMODnet Macrobenthos
+
+#-------------------------------------------------------------------------------*
+# ---- SET-UP ----
+#===============================================================================*
+
+# Load libraries:
+
+library(stringr)
+library(plyr)
+library(ggplot2)
+library(grid)
+library(gridExtra)
+library(MASS)
+
+# Source the functions file:
+
+getwd()
+setwd("C:/Users/auriemma/core-transient/")
+source('scripts/R-scripts/core-transient_functions.R')
+
+# Get data. First specify the dataset number ('ds') you are working with.
+
+ds = 85
+
+dataset = read.csv(paste('data/raw_datasets/dataset_', ds, '.csv', sep = ''))
+
+dataFormattingTable = read.csv('Reference/data_formatting_table.csv')
+#-------------------------------------------------------------------------------*
+# ---- EXPLORE THE DATASET ----
+#===============================================================================*
+
+names(dataset)
+head(dataset)
+str(dataset)
+summary(dataset)
+
+# Remove column 'X'
+
+dataset1 = dataset[,-1]
+head(dataset1)
+
+# Rename SampleID as site, and ID as datasetID
+
+names(dataset1)[1] = 'datasetID'
+names(dataset1)[3] = 'site'
+head(dataset1)
+tail(dataset1)
+
+# !GIT-ADD-COMMIT-PUSH AND DESCRIBE HOW THE DATA WERE MODIFIED!
+
+#!DATA FORMATTING TABLE UPDATE! 
+# Are the ONLY site identifiers the latitude and longitude of the observation or 
+# sample? (I.e., there are no site names or site IDs or other designations) Y/N
+
+dataFormattingTable[,'LatLong_sites'] = 
+  dataFormattingTableFieldUpdate(ds, 'LatLong_sites', 'Y')
+
