a38dd5811209aead679eb63b7cb7a4262dfabe83
unknown <auriemma@Bio-HurlbertL06-7479.ad.unc.edu>
2015-04-14 14:02:01 -0400

    completed scripts formatting 90

--- scripts/R-scripts/data_cleaning_scripts/dwork_90_mca.R
+++ scripts/R-scripts/data_cleaning_scripts/dwork_90_mca.R
@@ -259,3 +259,43 @@ head(dataset7)
 summary(dataset7)
 
 # !GIT-ADD-COMMIT-PUSH AND DESCRIBE HOW THE DATA WERE MODIFIED!
+
+#-------------------------------------------------------------------------------*
+# ---- UPDATE THE DATA FORMATTING TABLE AND WRITE OUTPUT DATA FRAMES  ----
+#===============================================================================*
+
+# Update the data formatting table
+
+dataFormattingTable = dataFormattingTableUpdate(ds, dataset7)
+
+# Final look at the dataset
+
+head(dataset7)
+
+summary(dataset7)
+
+# Everything looks good, write dataset to file
+
+write.csv(dataset7, "data/formatted_datasets/dataset_90.csv", row.names = F)
+
+# !GIT-ADD-COMMIT-PUSH THE FORMATTED DATASET IN THE DATA FILE, THEN GIT-ADD-COMMIT-PUSH THE UPDATED DATA FOLDER!
+
+# Update the format priority and format flag fields
+
+dataFormattingTable[,'format_priority'] = 
+  dataFormattingTableFieldUpdate(ds, 'format_priority', 'NA')
+
+dataFormattingTable[,'format_flag'] = 
+  dataFormattingTableFieldUpdate(ds, 'format_flag', 1)
+
+dataFormattingTable = dataFormattingTableUpdate(ds, dataset7)
+
+# And update the data formatting table:
+
+write.csv(dataFormattingTable, 'Reference/data_formatting_table.csv', row.names = F)
+
+# !GIT-ADD-COMMIT-PUSH THE DATA FORMATTING TABLE!
+
+# Remove all objects except for functions from the environment:
+
+rm(list = setdiff(ls(), lsf.str()))
