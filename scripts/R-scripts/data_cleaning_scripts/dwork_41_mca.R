e2e2a669cd8fd8e32c94022d660cb6b77fdc90f1
unknown <auriemma@Bio-HurlbertL06-7479.ad.unc.edu>
2015-04-21 15:45:21 -0400

    no changes to site, only one site in dataset

--- scripts/R-scripts/data_cleaning_scripts/dwork_41_mca.R
+++ scripts/R-scripts/data_cleaning_scripts/dwork_41_mca.R
@@ -58,4 +58,41 @@ head(dataset1)
 dataFormattingTable[,'LatLong_sites'] = 
   dataFormattingTableFieldUpdate(ds, 'LatLong_sites', 'N') 
 
+#-------------------------------------------------------------------------------*
+# ---- EXPLORE AND FORMAT SITE DATA ----
+#===============================================================================*
 
+# Explore
+
+head(dataset1, 20)
+summary(dataset1$site)
+
+# Only one site in this dataset, and no information about sites in the data.  
+# Site is "censusbreedingbirds_Neotoma"
+
+# No changes can be made to site data
+
+dataset2 = dataset1
+
+# !GIT-ADD-COMMIT-PUSH AND DESCRIBE HOW THE SITE DATA WERE MODIFIED!
+
+# !DATA FORMATTING TABLE UPDATE! 
+
+# Raw_siteUnit. How a site is coded (i.e. if the field was concatenated such as this one, it was coded as "site_block_treatment_plot_quad"). Alternatively, if the site were concatenated from latitude and longitude fields, the encoding would be "lat_long". 
+
+dataFormattingTable[,'Raw_siteUnit'] = 
+  dataFormattingTableFieldUpdate(ds, 'Raw_siteUnit','censusbreedingbirds_Neotoma') 
+
+# spatial_scale_variable. Is a site potentially nested (e.g., plot within a quad or decimal lat longs that could be scaled up)? Y/N
+
+dataFormattingTable[,'spatial_scale_variable'] = 
+  dataFormattingTableFieldUpdate(ds, 'spatial_scale_variable', 'NA')
+
+# Notes_siteFormat. Use this field to THOROUGHLY describe any changes made to the site field during formatting.
+
+dataFormattingTable[,'Notes_siteFormat'] = 
+  dataFormattingTableFieldUpdate(ds, 'Notes_siteFormat',  'no site information available here.  All sites are listed as censusbreedingbirds_Neotoma.  May need to get data again or re-check source.')
+
+#-------------------------------------------------------------------------------*
+# ---- EXPLORE AND FORMAT SPECIES DATA ----
+#===============================================================================*
