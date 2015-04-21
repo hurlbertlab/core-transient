c96e2e372969bcf61d9d78efaaf650591d1e2361
unknown <auriemma@Bio-HurlbertL06-7479.ad.unc.edu>
2015-04-21 14:52:26 -0400

    removed column and changed field names

---
+++ scripts/R-scripts/data_cleaning_scripts/dwork_39_mca.R
@@ -0,0 +1,64 @@
+# Formatting Dataset 39: ESA Birds
+
+#-------------------------------------------------------------------------------*
+# ---- SET-UP ----
+#===============================================================================*
+# Load libraries:
+
+library(stringr)
+library(plyr)
+library(ggplot2)
+library(grid)
+library(gridExtra)
+library(MASS)
+
+
+# Source the functions file:
+
+getwd()
+setwd('c:/Users/auriemma/core-transient/')
+source('scripts/R-scripts/core-transient_functions.R')
+
+# Get data. First specify the dataset number ('ds') you are working with.
+
+ds = 39 
+
+dataset = read.csv(paste('data/raw_datasets/dataset_', ds, '.csv', sep = ''))
+
+dataFormattingTable = read.csv('Reference/data_formatting_table.csv')
+
+#-------------------------------------------------------------------------------*
+# ---- EXPLORE THE DATASET ----
+#===============================================================================*
+
+names(dataset)
+head(dataset)
+summary(dataset)
+
+# Remove unused column 'X'
+
+dataset1 = dataset[,-1]
+
+# Change names of Species, ID, and SampleID fields
+
+names(dataset1)
+names(dataset1)[1] = "datasetID"
+names(dataset1)[3] = "site"
+names(dataset1)[4] = "species"
+
+# Check
+
+head(dataset1)
+
+# !GIT-ADD-COMMIT-PUSH AND DESCRIBE HOW THE DATA WERE MODIFIED!
+
+#!DATA FORMATTING TABLE UPDATE! 
+# Are the ONLY site identifiers the latitude and longitude of the observation or 
+# sample? (I.e., there are no site names or site IDs or other designations) Y/N
+
+dataFormattingTable[,'LatLong_sites'] = 
+  dataFormattingTableFieldUpdate(ds, 'LatLong_sites', 'N') 
+
+#-------------------------------------------------------------------------------*
+# ---- EXPLORE AND FORMAT SITE DATA ----
+#===============================================================================*
