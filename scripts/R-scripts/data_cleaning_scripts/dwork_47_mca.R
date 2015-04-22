f9a841b51bc14cfb4f257878eda4d18282712fa3
unknown <auriemma@Bio-HurlbertL06-7479.ad.unc.edu>
2015-04-22 11:09:12 -0400

    removed unused column X and removed years from site data

---
+++ scripts/R-scripts/data_cleaning_scripts/dwork_47_mca.R
@@ -0,0 +1,111 @@
+# Formatting Dataset 46: Ecological Data Wiki Birds
+
+#-------------------------------------------------------------------------------*
+# ---- SET-UP ----
+#===============================================================================*
+# Load libraries:
+
+library(stringr)
+library(plyr)
+library(ggplot2)
+library(grid)
+library(gridExtra)
+library(MASS)
+
+# Source the functions file:
+
+getwd()
+setwd('c:/Users/auriemma/core-transient/')
+source('scripts/R-scripts/core-transient_functions.R')
+
+# Get data. First specify the dataset number ('ds') you are working with.
+
+ds = 47 
+
+dataset = read.csv(paste('data/raw_datasets/dataset_', ds, '.csv', sep = ''))
+
+dataFormattingTable = read.csv('Reference/data_formatting_table.csv')
+
+#-------------------------------------------------------------------------------*
+# ---- EXPLORE THE DATASET ----
+#===============================================================================*
+
+names(dataset)
+head(dataset, 20)
+summary(dataset)
+
+# Remove unused field 'X'
+
+dataset1 = dataset[,-1]
+head(dataset1)
+
+# Change names of ID, SampleID and Species
+
+names(dataset1)[c(1,3,4)] = c('datasetID','site','species' )
+
+# Check
+
+names(dataset1)
+head(dataset1)
+
+# !GIT-ADD-COMMIT-PUSH AND DESCRIBE HOW THE DATA WERE MODIFIED!
+
+#!DATA FORMATTING TABLE UPDATE! 
+# Are the ONLY site identifiers the latitude and longitude of the observation or 
+# sample? (I.e., there are no site names or site IDs or other designations) Y/N
+
+dataFormattingTable[,'LatLong_sites'] = 
+  dataFormattingTableFieldUpdate(ds, 'LatLong_sites', 'N')
+
+#-------------------------------------------------------------------------------*
+# ---- EXPLORE AND FORMAT SITE DATA ----
+#===============================================================================*
+
+# Explore
+
+head(dataset1, 20)
+summary(dataset1$site)
+tail(dataset1)
+
+# Sites are listed as region_year, two regions included: Redvers and Lousana
+
+# Can remove the year portion of site strings because it is the same as Year field in the data 
+
+site = str_sub(dataset1$site, end = -7)
+unique(site)
+
+# Add new site field to dataset
+
+dataset2 = dataset1
+dataset2$site = site
+
+# check it out
+
+head(dataset2, 20)
+tail(dataset2,20)
+summary(dataset2)
+
+# All good
+
+# !GIT-ADD-COMMIT-PUSH AND DESCRIBE HOW THE SITE DATA WERE MODIFIED!
+
+# !DATA FORMATTING TABLE UPDATE! 
+
+# Raw_siteUnit. How a site is coded (i.e. if the field was concatenated such as this one, it was coded as "site_block_treatment_plot_quad"). Alternatively, if the site were concatenated from latitude and longitude fields, the encoding would be "lat_long". 
+
+dataFormattingTable[,'Raw_siteUnit'] = 
+  dataFormattingTableFieldUpdate(ds, 'Raw_siteUnit','region_year') 
+
+# spatial_scale_variable. Is a site potentially nested (e.g., plot within a quad or decimal lat longs that could be scaled up)? Y/N
+
+dataFormattingTable[,'spatial_scale_variable'] = 
+  dataFormattingTableFieldUpdate(ds, 'spatial_scale_variable', 'N')
+
+# Notes_siteFormat. Use this field to THOROUGHLY describe any changes made to the site field during formatting.
+
+dataFormattingTable[,'Notes_siteFormat'] = 
+  dataFormattingTableFieldUpdate(ds, 'Notes_siteFormat',  'site data included two sites followed by year: Redvers_year and Lousana_year.  Used substring to remove the year from the site leaving just the site in the data.')
+
+#-------------------------------------------------------------------------------*
+# ---- EXPLORE AND FORMAT SPECIES DATA ----
+#===============================================================================*
