35706c01d7b267ee64731c576172cfdc0bd9b9b3
unknown <auriemma@Bio-HurlbertL06-7479.ad.unc.edu>
2015-04-27 10:52:56 -0400

    removed unused column and changed field names

---
+++ scripts/R-scripts/data_cleaning_scripts/dwork_99_mca.R
@@ -0,0 +1,63 @@
+# Formatting Dataset 99: OBIS Tropical Fish
+
+#-------------------------------------------------------------------------------*
+# ---- SET-UP ----
+#===============================================================================*
+
+# Load libraries:
+
+library(stringr)
+library(plyr)
+library(ggplot2)
+library(grid)
+library(gridExtra)
+library(MASS)
+
+# Source the functions file:
+
+getwd()
+setwd("C:/Users/auriemma/core-transient/")
+source('scripts/R-scripts/core-transient_functions.R')
+
+# Get data. First specify the dataset number ('ds') you are working with.
+
+ds = 99
+
+dataset = read.csv(paste('data/raw_datasets/dataset_', ds, '.csv', sep = ''))
+
+dataFormattingTable = read.csv('Reference/data_formatting_table.csv')
+
+#-------------------------------------------------------------------------------*
+# ---- EXPLORE THE DATASET ----
+#===============================================================================*
+
+names(dataset)
+head(dataset)
+tail(dataset)
+summary(dataset)
+
+# Remove unused column 'X'
+
+dataset1 = dataset[,-1]
+head(dataset1)
+
+# Rename SampleID as site, and ID as datasetID
+
+names(dataset1)[c(1,3,4)] = c('datasetID','site','species')
+
+# Check
+
+head(dataset1)
+
+# !GIT-ADD-COMMIT-PUSH AND DESCRIBE HOW THE DATA WERE MODIFIED!
+
+#!DATA FORMATTING TABLE UPDATE! 
+# Are the ONLY site identifiers the latitude and longitude of the observation or 
+# sample? (I.e., there are no site names or site IDs or other designations) Y/N
+
+dataFormattingTable[,'LatLong_sites'] = 
+  dataFormattingTableFieldUpdate(ds, 'LatLong_sites', 'N')
+
+#-------------------------------------------------------------------------------*
+# ---- EXPLORE AND FORMAT SITE DATA ----
+#===============================================================================*
