fa2db85d45bd50c43f665d40e5cb6132f83f8a64
Will Larsen <wlarsen@live.unc.edu>
2015-06-02 16:19:53 -0400

    Formatted species entries for dataset 243. Removed unidentified species and combined male and female entries for Semicossyphus pulcher into one

--- scripts/R-scripts/data_cleaning_scripts/dwork_243_wjl.R
+++ scripts/R-scripts/data_cleaning_scripts/dwork_243_wjl.R
@@ -167,50 +167,38 @@ head(dataset4)
 dataFormattingTable[,'countFormat'] = 
   dataFormattingTableFieldUpdate(ds, 'countFormat',    # Fill value below in quotes
                                  
-                                 'cover')
+                                 'density')
 
 dataFormattingTable[,'Notes_countFormat'] = 
   dataFormattingTableFieldUpdate(ds, 'Notes_countFormat', # Fill value below in quotes
                                  
-                                 'Data represents cover. There were no NAs nor 0s that required removal')
+                                 'Data represents density. In the raw data, adult and juvenile densities were in separate columns. These were added together in count column.')
 
 #-------------------------------------------------------------------------------*
 # ---- EXPLORE AND FORMAT SPECIES DATA ----
 #===============================================================================*
-# Here, your primary goal is to ensure that all of your species are valid. To do so, you need to look at the list of unique species very carefully. Avoid being too liberal in interpretation, if you notice an entry that MIGHT be a problem, but you can't say with certainty, create an issue on GitHub.
 
-# Look at the individual species present:
+dataset4$species = factor(dataset4$species)
 
 levels(dataset4$species) 
-
-# The first thing that I notice is that there are lower and upper case entries. Because R is case-sensitive, this will be coded as separate species. Modify this prior to continuing:
-
-dataset4$species = factor(toupper(dataset4$species))
 
 # Now explore the listed species themselves, again. A good trick here to finding problematic entries is to shrink the console below horizontally so that species names will appear in a single column.  This way you can more easily scan the species names (listed alphabetically) and identify potential misspellings, extra characters or blank space, or other issues.
 
 levels(dataset4$species)
 
-# If species names are coded (not scientific names) go back to study's metadata to learn what species should and shouldn't be in the data. 
+# Removing entries of 'Sebastes spp.' 
+# These could refer to any of the 11 species of Sebastes found in the study.
+# There were other entries with unidentified species, but each had a unique genus, so there was no overlap like with Sebastes.
+# Their species codes were 1031, 1032, 1034, 1038, 1040, 1052, 1060
 
-# In this example, a quick look at the metadata is not informative, unfortunately. Because of this, you should really stop here and post an issue on GitHub. With some more thorough digging, however, I've found the names represent "Kartez codes". Several species can be removed (double-checked with USDA plant codes at plants.usda.gov and another Sevilleta study (dataset 254) that provides species names for some codes). Some codes were identified with this pdf from White Sands: https://nhnm.unm.edu/sites/default/files/nonsensitive/publications/nhnm/U00MUL02NMUS.pdf
-
-bad_sp = c('','NONE','UK1','UKFO1','UNK1','UNK2','UNK3','LAMIA', 'UNGR1','CACT1','UNK','NONE','UNK2','UNK3', 'UNK1','FORB7', 'MISSING', '-888', 'DEAD','ERRO2', 'FORB1','FSEED', 'GSEED', 'MOSQ', 'SEED','SEEDS1','SEEDS2', 'SEFLF','SESPM','SPOR1')
+bad_sp = c('1051')
 
 dataset5 = dataset4[!dataset4$species %in% bad_sp,]
 
-# It may be useful to count the number of times each name occurs, as misspellings or typos will likely
-# only show up one time.
+# The codes 1006 and 1008 are the female and male entries for Semicossyphus pulcher. These can be combined, as we are only looking at species, not sex.
+# The code 1006 now represents all individuals of species Semicossyphus pulcher
 
-table(dataset5$species)
-
-# If you find any potential typos, try to confirm that the "mispelling" isn't actually a valid name.
-# If not, then go ahead and replace all instances like this:
-
-typo_name = ''
-good_name = ''
-
-dataset5$species[dataset$species == typo_name] = good_name
+dataset5$species[dataset5$species == '1008'] = '1006'
 
 
 # Reset the factor levels:
