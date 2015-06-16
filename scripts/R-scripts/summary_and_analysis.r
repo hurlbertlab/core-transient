9ce7c673d4ae437cdad74677b4553cee46c0116e
Allen Hurlbert <hurlbert@bio.unc.edu>
2015-06-16 16:09:34 -0400

    dashboard script for creating summary stats for datasets

---
+++ scripts/R-scripts/summary_and_analysis.r
@@ -0,0 +1,51 @@
+###############################################
+# Code for running core-transient analysis
+# and data summaries over all formatted datasets.
+#
+# Input files are named propOcc_XXX.csv where
+# XXX is the dataset ID.
+
+source('scripts/R-scripts/core-transient_functions.R')
+
+# Maximum occupancy of transient species
+# (and hence the minimum occupancy of core species is 1 - threshold)
+threshold = 1/3
+
+# Number of replicates for randomization tests
+reps = 999
+
+##################################################################
+
+# If running summaries for the first time (or wanting to start
+# anew because all formatted datasets have changed) and a
+# 'core-transient_summary.csv' file does not exist yet in the
+# output/tabular_data folder, or if you just want to get summary
+# stats for one or a few datasets into R, run this section
+
+# Specify here the datasetIDs and then run the code below.
+datasetIDs = c()
+
+summaries = c()
+for (d in datasetIDs) {
+  newsumm = summaryStatsFun(d, threshold, reps)
+  summaries = rbind(summaries, newsumm)
+}
+
+write.csv(summaries, 'output/tabular_data/core-transient_summary.csv', 
+          row.names = F)
+
+##################################################################
+
+##################################################################
+
+# If running summaries for the newly updated or created formatted
+# datasets to be appended to the existing 'core-transient_summary.csv'
+# file, then run this section.
+
+# If you do not want to re-write the existing file, set write = FALSE.
+
+# Also, this command can be used instead of the section above to
+# create the 'core-transient_summary.csv' file from scratch for all
+# datasets with formatted data.
+
+addNewSummariesFun(threshold, reps, write = TRUE)
