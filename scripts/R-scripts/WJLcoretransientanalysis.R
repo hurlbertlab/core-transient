951cc56bad4d909716862d1f83905cfa0452771e
unknown <wlarsen@live.unc.edu>
2015-07-27 16:31:16 -0400

    worked on Chile mammals project, began plotting vars

--- scripts/R-scripts/WJLcoretransientanalysis.R
+++ scripts/R-scripts/WJLcoretransientanalysis.R
@@ -1,6 +1,10 @@
 ##########################################################
 # Dataset 236 Chilean Small Mammals  Core-transient analysis
-# Metadata can be found at http://esapubs.org/archive/ecol/E094/084/metadata.php
+# Metadata for species occupancy: http://esapubs.org/archive/ecol/E094/084/metadata.php
+# Metadata for dryad dataset: http://datadryad.org/resource/doi:10.5061/dryad.sb175.2
+# Additional metadata for dryad: http://onlinelibrary.wiley.com/doi/10.1111/mec.12572/full
+# Metadata for Ernest life history dataset: http://www.esapubs.org/archive/ecol/E084/093/default.htm#data
+# Metadata for pantheria dataset: http://esapubs.org/Archive/ecol/E090/184/metadata.htm
 ##########################################################
 
 # setwd('C:/Users/wlarsen/Desktop')
@@ -15,7 +19,7 @@ goodsites = c(3,6,11,14)
 
 dataset1 = dataset[dataset$site %in% goodsites,]
 
-layout(matrix(c(6,1,2,3,4,5), 3,2, byrow = TRUE))
+# layout(matrix(c(6,1,2,3,4,5), 3,2, byrow = TRUE))
 hist(dataset$propOcc)
 hist(dataset$propOcc[dataset1$site == 3])
 hist(dataset$propOcc[dataset1$site == 6])
@@ -71,26 +75,98 @@ for (i in 1:length(avgdataset$speclist)){
 # Plot
 barplot(avgdataset$avgpropOcc, names.arg = avgdataset$speccodes)
 
-# Dryad dataset
+### Dryad dataset
 dryad = read.csv('Data4Dryad.csv')
-dryadm = dryad[dryad$TAXON %in% 'mammals',]
-dryadm$SPECIES = gsub("_"," ",dryadm$SPECIES)
-dryadmsp = dryadm[dryadm$SPECIES %in% speclist,]
-dryadmsp = dryadmsp[c(4,5,1,6,2,8,3,7),]
-dryadmsp = dryadmsp[-1]
+dryadsp = dryad[dryad$TAXON %in% 'mammals',]
+dryadsp$SPECIES = gsub("_"," ",dryadm$SPECIES)
+# Extract needed species
+dryadsp = dryadm[dryadsp$SPECIES %in% speclist,]
+# Remove unneccessary columns
+dryadsp = dryadsp[-1]
+head(dryadsp)
 # Does not have an entry for Octodon degus
 
-# Ernest life history dataset
+
+
+### Ernest life history dataset
 lifehist = read.csv('Mammal_lifehistories_v2.csv')
 lifehist$scientificname = paste(lifehist$Genus,lifehist$species,sep=" ")
 lifehist = lifehist[c(15,5:14)]
-lifehistsp = lifehist[lifehist$scientificname %in% speclist,]
-# Does not have an entry for Abrothrix genus
 
+# This dataset has 'Abrocoma bennetti', it should be 'Abrocoma bennettii'
+lifehist$scientificname[lifehist$scientificname == 'Abrocoma bennetti'] = 'Abrocoma bennettii'
+
+# Extract needed species
+lifehistsp = lifehist[lifehist$scientificname %in% speclist,]
+head(lifehistsp)
+
+# In this dataset, if a value is not available, a -999 is put in its place, replacing with NA's
+lifehistsp[lifehistsp == -999] = NA_character_
+
+# Remove unneccessary column:
+lifehistsp = lifehistsp[-11]
+
+head(lifehistsp)
+# Does not have an entry for Abrothrix genus, Thylamys elegans, or Chelemys megalonyx
+
+
+### Pantheria dataset
 pantheria = read.csv('PanTHERIA_1-0_WR05_Aug2008.csv')
+
+# Extract needed species
 pantheria.sp = pantheria[pantheria$MSW05_Binomial %in% speclist,]
-pantheria.sp = pantheria.sp[-c(1:4)]
-# Reorder according to speclist
-pantheria.sporder = pantheria.sp[c(1,4,6,2,7,9,8,3,5),]
+
+# In this dataset, if a value is not available, a -999 is put in its place, replacing with NA's
+pantheria.sp[pantheria.sp == -999] = NA_character_
+
+# Many columns unnecessary:
+unusedfields = c('MSW05_Order','MSW05_Family','MSW05_Genus','MSW05_Species','References')
+
+# Also removing columns with all NA's (no usable data):
+for (i in 1:dim(pantheria.sp)[2]){
+  
+  if(sum(is.na(pantheria.sp[,i]))==9){
+    
+    unusedfields = c(unusedfields, names(pantheria.sp)[i])
+    
+  }
+  
+}
+unusedfieldsind = which(names(pantheria.sp) %in% unusedfields)
+pantheria.sp = pantheria.sp[-unusedfieldsind]
+
+head(pantheria.sp)
+
+### Merging the three datasets
+# Merging dryad and life history datasets first
+merge1= merge(dryadsp,lifehistsp, by.x = "SPECIES", by.y = "scientificname", all=TRUE)
+# Check:
+dim(dryadsp)
+dim(lifehistsp)
+dim(merge1)
+# Merging first merge dataset with pantheria.sp
+merge2 = merge(merge1,pantheria.sp, by.x = "SPECIES", by.y = "MSW05_Binomial", all=TRUE)
+dim(merge1)
+dim(pantheria.sp)
+dim(merge2)
+# Merge with avgdataset
+datasetm = merge(avgdataset,merge2,by.x = "speclist",by.y="SPECIES",all=TRUE)
+# Check:
+dim(avgdataset)
+dim(merge2)
+dim(datasetm)
+
+
+plot(datasetm$avgpropOcc,datasetm$ENV.HARSHNESS)
+abline(lm(datasetm$ENV.HARSHNESS~datasetm$avgpropOcc))
+
+plot(datasetm$avgpropOcc,datasetm$GEOGRAPHIC.COVERAGE)
+abline(lm(datasetm$GEOGRAPHIC.COVERAGE~datasetm$avgpropOcc))
+
+plot(datasetm$avgpropOcc,datasetm$RAINFALL.UNPREDICTABILITY)
+abline(lm(datasetm$RAINFALL.UNPREDICTABILITY~datasetm$avgpropOcc))
+
+plot(datasetm$avgpropOcc,datasetm$RESIDUAL.BODY.SIZE)
+abline(lm(datasetm$RESIDUAL.BODY.SIZE~datasetm$avgpropOcc))
 
 
