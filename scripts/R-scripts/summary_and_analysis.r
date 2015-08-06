d50034b3c47ef3ceced3f9e87d53d3b2c252f5ee
Allen Hurlbert <hurlbert@bio.unc.edu>
2015-08-06 16:56:39 -0400

    integrate occ_dist_vs_scale and summary_and_analysis scripts, in progress

--- scripts/R-scripts/summary_and_analysis.r
+++ scripts/R-scripts/summary_and_analysis.r
@@ -189,38 +189,13 @@ plot(trandist, type = 'l', lwd = 6, xaxt = "n", yaxt = "n", xlab = "Occupancy",
 # Summary of other distribution statistics by taxa
 
 par(mfrow = c(1,1), mar = c(6, 6, 1, 1), mgp = c(4, 1, 0))
-summ4$taxa = with(summ4, reorder(taxa, bimodality, function(x) mean(x, na.rm = T)))
-boxplot(summ4$bimodality ~ summ4$taxa, cex.axis = 1.25, ylab = "Bimodality", boxwex = .6)
-summ5 = summ4
-summ5$taxa = with(summ5, reorder(taxa, pBimodal, function(x) mean(x, na.rm = T)))
-boxplot(summ5$pBimodal ~ summ5$taxa, cex.axis = 1.25, ylab = "p (Bimodal)", boxwex = .6)
+summ3$taxa = with(summ3, reorder(taxa, bimodality, function(x) mean(x, na.rm = T)))
+boxplot(summ3$bimodality ~ summ3$taxa, cex.axis = 1.25, ylab = "Bimodality", boxwex = .6)
+summ4 = summ3
+summ4$taxa = with(summ4, reorder(taxa, pBimodal, function(x) mean(x, na.rm = T)))
+boxplot(summ4$pBimodal ~ summ4$taxa, cex.axis = 1.25, ylab = "p (Bimodal)", boxwex = .6)
 
 
-#####################################################
-# Boxplots showing distribution of core and transient
-# species by system (terrestrial/marine).
-
-meanCoreBySystem = aggregate(summ$propCore, by = list(summ$system), mean)
-uniqsystem = meanCoreBySystem$Group.1[order(meanCoreBySystem$x, decreasing = T)]
-
-par(mfrow = c(1,1), mar = c(5, 5, 1, 1), mgp = c(3, 1, 0))
-boxplot(summ$propCore, xlim = c(0.25, 4.75), ylim = c(0, 1.2), 
-        border = 'white', col = 'white', ylab = "Fraction of species", cex.lab = 2, las = 1, 
-        cex.axis = 1.25)
-for (i in 1:length(uniqsystem)) {
-  syst = uniqsystem[i]
-  boxplot(summ$propTrans[summ$system == syst], add = T, col = transCol, #width = 0.1, 
-          at = 3*(i-1)+.5, yaxt = "n")
-  boxplot(summ$propNeither[summ$system == syst], add =T, col = nonCol, #width = 0.2,
-          at = 3*(i-1)+1, yaxt = "n")
-  boxplot(summ$propCore[summ$system == syst], add =T, col = coreCol, #width = 1,
-          at = 3*(i-1)+1.5, yaxt = "n")
-}
-axis(1, uniqsystem, at = 3*(1:2 - 1)+1, cex.axis = 2)
-rect(.2, 1.1, .45, 1.2, col = transCol, border=F)
-rect(1.5, 1.1, 1.75, 1.2, col = nonCol, border=F)  
-rect(2.7, 1.1, 2.95, 1.2, col = coreCol, border=F)  
-text(c(.9, 2.1, 3.15), c(1.15, 1.15, 1.15), c('Transient', 'Neither', 'Core'), cex = 1.75)
 
 
 #########################################################
@@ -254,58 +229,6 @@ fiftyMD1 = subset(fifty, stateroute %in% unique(md10.rt.occ$stateroute) & year >
                   select = c('stateroute','year','AOU','Stop1'))
 md1.rt.occ = data.frame(table(fiftyMD1[,c('stateroute','AOU')])/15)
 md1.rt.occ2 = md1.rt.occ[md1.rt.occ$Freq!=0,]
-
-
-fiftyMD1 = subset(fifty, stateroute %in% unique(md10.rt.occ$stateroute) & year > 1995 & year < 2011 & Stop1!=0, 
-                  select = c('stateroute','year','AOU','Stop1'))
-md1.rt.occ = data.frame(table(fiftyMD1[,c('stateroute','AOU')])/15)
-md1.rt.occ2 = md1.rt.occ[md1.rt.occ$Freq!=0,]
-
-
-# OTHER REGIONS ##########################################################################
-
-# CA/OR BBS data
-ca.counts = subset(counts5, statenum==14 | statenum == 69)
-ca.occ.mat = occupancy.matrix[floor(as.numeric(row.names(occupancy.matrix))/1000)==14 |
-                                floor(as.numeric(row.names(occupancy.matrix))/1000)==69,]
-ca.uniq = unique(ca.counts[,c('Year','Aou')])
-# CA/OR statewide temporal occupancy (27 routes)
-ca.occ = data.frame(table(ca.uniq$Aou)/15)
-
-#Scale of 10 BBS point count stops (specifically stops 1-10)
-ca10 = unique(ca.counts[ca.counts$Count10!=0,c('stateroute','Year','Aou')])
-ca10.rt.occ = data.frame(table(ca10[,c('stateroute','Aou')])/15)
-ca10.rt.occ2 = ca10.rt.occ[ca10.rt.occ$Freq!=0,]
-
-# Scale of 1 BBS point count stop
-fiftyca1 = subset(fifty, stateroute %in% unique(ca10.rt.occ$stateroute) & year > 1995 & year < 2011 & Stop1!=0, 
-                  select = c('stateroute','year','AOU','Stop1'))
-ca1.rt.occ = data.frame(table(fiftyca1[,c('stateroute','AOU')])/15)
-ca1.rt.occ2 = ca1.rt.occ[ca1.rt.occ$Freq!=0,]
-
-
-
-#Colorado route
-routesCO = subset(routes, statenum == 17)
-routesCO_SWcorner = subset(routesCO, Longi < -106 & Lati < 39)
-
-co.counts = subset(counts5, stateroute %in% routesCO_SWcorner$stateroute)
-co.occ.mat = occupancy.matrix[as.numeric(row.names(occupancy.matrix)) %in% routesCO_SWcorner$stateroute,]
-
-co.uniq = unique(co.counts[,c('Year','Aou')])
-co.occ = data.frame(table(co.uniq$Aou)/15)
-
-#Scale of 10 BBS point count stops (specifically stops 1-10)
-co10 = unique(co.counts[co.counts$Count10!=0,c('stateroute','Year','Aou')])
-co10.rt.occ = data.frame(table(co10[,c('stateroute','Aou')])/15)
-co10.rt.occ2 = co10.rt.occ[co10.rt.occ$Freq!=0,]
-
-#Scale of 1 BBS point count stop (specifically stop 1)
-
-fiftyCO1 = subset(fifty, stateroute %in% unique(co10.rt.occ$stateroute) & year > 1995 & year < 2011 & Stop1!=0, 
-                  select = c('stateroute','year','AOU','Stop1'))
-co1.rt.occ = data.frame(table(fiftyCO1[,c('stateroute','AOU')])/15)
-co1.rt.occ2 = co1.rt.occ[co1.rt.occ$Freq!=0,]
 
 ########################################################################################
 
@@ -384,6 +307,16 @@ dev.off()
 
 
 
+####################################################################################
+# Mean occupancy values at each scale
+meanOcc.allBBS = mean(BBS.occ$Freq)
+meanOcc.MD = mean(md.occ$Freq)
+meanOcc.singleBBS = mean(md.occ.mat[!is.na(md.occ.mat)])
+meanOcc.MD10 = mean(md10.rt.occ2$Freq)
+meanOcc.MD01 = mean(md1.rt.occ2$Freq)
+
+meanOcc = c(meanOcc.allBBS, meanOcc.MD, meanOcc.singleBBS, meanOcc.MD10, meanOcc.MD01)
+
 
 ####################################################################################
 # Mean spatial scale of each of the BBS scales
@@ -393,6 +326,8 @@ meanSpatScale = c(scale.1stop*50*497, #area of 497 BBS routes
                   scale.1stop*50,     #area of 1 BBS route
                   scale.1stop*10,     #area of 10 point count stops
                   scale.1stop)        #area of 1 point count stop
+
+pdf('output/plots/occ_vs_spatialScale_BBS.pdf', height = 6.06*8/7, width = 8)
 par(mar = c(6, 6, 1, 1), mgp = c(4, 1, 0), cex.lab = 2.5)
 plot(log10(meanSpatScale), meanOcc, 
      xlab = expression(paste(plain(log)[10]," Spatial scale (", plain(km)^2, ")")), 
@@ -402,7 +337,7 @@ legend('topleft',
        c('Maryland (27 BBS routes)',
          'Single BBS route (50 stops)','10 point count stops','1 point count stop'),
        col = c(col1, col2, col3, col4), pch = 16, cex = 1.5, pt.cex = 2)
-
+dev.off()
 
 #####################################################################################
 # Get mean community size (per year) for each of the above scales of BBS data
@@ -416,33 +351,17 @@ meanN.MD01 = sum(fiftyMD1$Stop1)/15/numMDroutes
 
 meanN = c(meanN.allBBS, meanN.MD, meanN.singleBBS, meanN.MD10, meanN.MD01)
 
-meanOcc.allBBS = mean(BBS.occ$Freq)
-meanOcc.MD = mean(md.occ$Freq)
-meanOcc.singleBBS = mean(md.occ.mat[!is.na(md.occ.mat)])
-meanOcc.MD10 = mean(md10.rt.occ2$Freq)
-meanOcc.MD01 = mean(md1.rt.occ2$Freq)
+#########
+#########
+# TO DO #
+############################
+# Figure out appropriate scale/dimensions for the figure below
+# (and therefore immediately above) and create a figure that
+# plots dataset means.
 
-meanOcc = c(meanOcc.allBBS, meanOcc.MD, meanOcc.singleBBS, meanOcc.MD10, meanOcc.MD01)
 
 # Get summary data for all other datasets
-summ = read.csv('output/tabular_data/core-transient_summary.csv')
-
-colors7 = c(rgb(29/255, 106/255, 155/255),
-            colors()[612],
-            colors()[552],
-            colors()[144],
-            rgb(0, 54/255, 117/255),
-            rgb(86/255, 141/255, 27/255),
-            colors()[547])
-
-symbols7 = c(16:18,15, 17, 167,18)
-
-taxcolors = data.frame(taxa = unique(summ$taxa), color = colors7, pch = symbols7)
-summ2 = merge(summ4, taxcolors, by = 'taxa', all.x = T)
-summ2$color = as.character(summ2$color)
-summ3 = subset(summ2, !datasetID %in% c(1, 99))
-bbssumm = subset(summ2, datasetID==1)
-
+pdf('output/plots/occ_vs_communitySize_allDatasets.pdf', height = 6.06*8/7, width = 8)
 par(mar = c(6, 6, 1, 1), las = 1)
 plot(log10(meanN), meanOcc, xlab = expression(paste(plain(log)[10]," Community Size")), 
      ylab = 'Mean occupancy', pch = 16, col = c('black', col1, col2, col3, col4), 
@@ -450,11 +369,11 @@ plot(log10(meanN), meanOcc, xlab = expression(paste(plain(log)[10]," Community S
 points(log10(bbssumm$meanAbundance), bbssumm$mu, pch = 16, cex = 2, col = colors7[1])
 points(log10(meanN), meanOcc, pch = 16, col = c('black', col1, col2, col3, col4), cex = 4)
 
-points(log10(summ3$meanAbundance), summ3$mu, pch = summ3$pch, cex = 2, col = summ3$color, font = 5)
+points(log10(notbbs$meanAbundance), notbbs$mu, pch = summ3$pch, cex = 2, col = summ3$color, font = 5)
 legend('topleft', legend = unique(summ$taxa), pch = symbols7, 
        col = c(colors7[1:5], 'white', colors7[7]), pt.cex = 2, cex = 1.5)
 points(0.76, 0.85, pch = symbols7[6], font = 5, col = colors7[6], cex = 2)
-
+dev.off()
 
 
 ##########################################################################
