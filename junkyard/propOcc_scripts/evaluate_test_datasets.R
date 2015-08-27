5458ef7ed08fbe9cd2a2607a58752693e2d6e398
Allen Hurlbert <hurlbert@bio.unc.edu>
2015-08-27 13:48:07 -0400

    created testResult output and new test dataset

--- junkyard/propOcc_scripts/evaluate_test_datasets.R
+++ junkyard/propOcc_scripts/evaluate_test_datasets.R
@@ -5,7 +5,14 @@ library('tidyr')
 source('scripts/R-scripts/core-transient_functions.R')
 
 #---------------------------------------------------------------------
-# Function to create fake datasets
+# Function to create fake datasets in which plots are sampled within sites, and
+# multiple sampling dates may be sampled each year (for simplicity, these
+# are just different dates within the month of January).
+
+# Fake datasets in which spatial subsampling (nPlots) varies over time, or
+# temporal subsampling (nDatesPerYear) varies across sites/plots can be
+# built by rbinding two separate calls of fakeData with different parameters.
+# (see below)
 
 fakeData = function(nSites, nPlots, nYears, nDatesPerYear, 
                     startingSite = 1,
@@ -121,40 +128,78 @@ testData6 = rbind(d6a, d6b, d6c)
 # Expected output: w = 6, z = 4
 #                  16 years dropped, site 2 dropped
 
-d7a = fakeData(nSites = 1, nPlots = 6, nYears = 10, nDatesPerYear = 4)
-d7b = fakeData(nSites = 1, nPlots = 2, nYears = 10, nDatesPerYear = 4, 
+d7a = fakeData(nSites = 1, nPlots = 6, nYears = 6, nDatesPerYear = 4)
+d7b = fakeData(nSites = 1, nPlots = 2, nYears = 6, nDatesPerYear = 4, 
                startingYear = 2007)
-d7c = fakeData(nSites = 1, nPlots = 1, nYears = 10, nDatesPerYear = 4,
+d7c = fakeData(nSites = 1, nPlots = 1, nYears = 6, nDatesPerYear = 4,
                startingSite = 2, startingYear = 2013)
 
 testData7 = rbind(d7a, d7b, d7c)
 
-# Expected values of w and z for the test datasets above:
-wShouldBe = c(4, 6, 6, 6, 6, 2, 6)
-zShouldBe = c(4, 4, 2, 2, 6, 4, 4)
 
-testDatasets = ls()[grep('testData', ls())]
-wResults = vector(length = length(testDatasets))
-zResults = c()
-for (i in 1:length(testDatasets)) {
-  dataset = get(testDatasets[i])
+# Dataset 8: half-1 of sites with high spatial subsampling but low temporal
+#            and half+1 have low spatial subsampling and high temporal subsampling
+# Expected output: w = 2, z = 6   since this yields more sites than w = 6, z = 2
+#                  3/8 sites dropped
+
+
+d8a = fakeData(nSites = 3, nPlots = 6, nYears = 10, nDatesPerYear = 2)
+d8b = fakeData(nSites = 5, nPlots = 2, nYears = 10, nDatesPerYear = 6,
+               startingSite = 4)
+
+testData8 = rbind(d8a, d8b)
+
+
+# Expected values of w and z and the number of sites kept for the test datasets above:
+# BE SURE TO ADD EXPECTED VALUES TO THESE VECTORS FOR EVERY NEW FAKE DATASET CREATED.
+wShouldBe = c(4, 6, 6, 6, 6, 2, 6, 2)
+zShouldBe = c(4, 4, 2, 2, 6, 4, 4, 8)
+numSitesShouldBe = c(4, 2, 2, 5, 1, 1, 1, 5)
+numYearsShouldBe = c(10, 10, 10, 10, 10, 12, 3, 10)
+
+#-----------------------------------------------------------------------------------
+# Conduct tests
+
+testingData = ls()[grep('testData', ls())]
+w = vector(length = length(testingData))
+z = vector(length = length(testingData))
+numSites = vector(length = length(testingData))
+numYears = vector(length = length(testingData))
+PassFail = vector(length = length(testingData))
+for (i in 1:length(testingData)) {
+  dataset = get(testingData[i])
   inData = richnessYearSubsetFun(dataset, spatialGrain, temporalGrain, minNTime, 
                                   minSpRich, dataDescription)
   
   zOut = zFinder(inData, minNTime, proportionalThreshold)
   wOut = wFinder(inData, minNTime, proportionalThreshold)
-  if (zout$z == zShouldBe[i]) {
-    zResults[i] = "PASS"
+  
+  subsettedData = wzSubsetFun(inData, minNTime, proportionalThreshold)
+  
+  z[i] = zOut$z
+  w[i] = wOut$w
+  numSites[i] = length(unique(subsettedData$site))
+  numYears[i] = length(unique(subsettedData$year))
+  
+  if (z[i] == zShouldBe[i] &
+      w[i] == wShouldBe[i] &
+      numSites[i] == numSitesShouldBe[i] &
+      numYears[i] == numYearsShouldBe[i]) { 
+    PassFail[i] = "PASS"
   } else {
-    zResults[i] = "FAIL"
+    PassFail[i] = "FAIL"
   }
 }
-#-------------------------
-inData1 = richnessYearSubsetFun(testData1, spatialGrain, temporalGrain, minNTime, 
-                                minSpRich, dataDescription)
-
-zOut1 = zFinder(inData1, minNTime, proportionalThreshold)
-wOut1 = wFinder(inData1, minNTime, proportionalThreshold)
+testResult = data.frame(dataset = testingData, 
+                        wShouldBe,
+                        zShouldBe,
+                        numSitesShouldBe,
+                        numYearsShouldBe,
+                        w, 
+                        z, 
+                        numSites,
+                        numYears,
+                        PassFail)
 
 
 
