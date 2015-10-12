14f21bf06c7b7002a307f8b81cfba1ba2da879f7
Ethan White <ethan@weecology.org>
2015-10-12 19:32:33 -0400

    Start script for text mining data from the BBC

---
+++ bbc-text-mining.py
@@ -0,0 +1,77 @@
+"""Extract text data from Breeding Bird Census pdfs"""
+
+import os
+import re
+import string
+from glob import glob
+
+def convert_pdf_to_images(filename):
+    """Convert a pdf to images"""
+    filename = os.path.splitext(filename)[0]
+    os.system("convert -density 350 {0}.pdf {0}.png".format(filename))
+
+def ocr(filename):
+    """OCR a file using tesseract"""
+    filename = os.path.splitext(filename)[0]
+    os.system("tesseract {0}.png {0}".format(filename))
+
+def convert_pdfs_to_text(path):
+    """Convert all PDFs in a directory to text
+
+    Use convert to convert to images and tesseract for OCR
+    
+    """
+    pdfs = glob(os.path.join(path, "*.pdf"))
+    for pdf in pdfs:
+        convert_pdf_to_images(pdf)
+        
+        #multi-page pdfs create multiple png files so loop over them
+        pngs = glob(os.path.join(path, "*.png"))
+        for png in pngs:
+            ocr(png)
+
+def cleanup_nonpara_pages(path, para_starts):
+    """Remove text and png files for pages that aren't the core paragraph data"""
+    for year in para_starts:
+        pages  = range(para_starts[year] - 1) #pages are not zero indexed
+        for page in pages:
+            os.remove(os.path.join(path, "BBC{}-{}.txt".format(year, page)))
+            os.remove(os.path.join(path, "BBC{}-{}.png".format(year, page)))
+
+def combine_txt_files_by_yr(path, years):
+    """Combine multiple text files into a single file for each year
+
+    File names have the general format: BBC1988-0.txt
+    
+    """
+    for year in years:
+        with open(os.path.join(path, "bbc_combined_{}.txt".format(year)), 'w') as outfile:
+            filenames = glob(os.path.join(path, "BBC{}*.txt".format(year)))
+            sorted_filenames = sorted_nicely(filenames)
+            for fname in sorted_filenames:
+                with open(fname) as infile:
+                    outfile.write(infile.read())
+
+def sorted_nicely(l): 
+    """ Sort the given iterable in the way that humans expect.
+
+    From:
+    http://stackoverflow.com/questions/2669059/how-to-sort-alpha-numeric-set-in-python
+    
+    """ 
+    convert = lambda text: int(text) if text.isdigit() else text 
+    alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ] 
+    return sorted(l, key = alphanum_key)
+
+para_starts = {1988: 4, 1989: 6, 1990: 6, 1991: 6,
+               1992: 7, 1993: 7, 1994: 7, 1995: 6}
+data_path = "./data/raw_datasets/BBC_pdfs/"
+convert_pdfs_to_text(data_path)
+cleanup_nonpara_pages(data_path, para_starts)
+combine_txt_files_by_yr(data_path, para_starts.keys())
+
+p = re.compile(r'((?:Location|Continuity|Size|Description of Plot|Edge|Topograph and Elevation|Weather|Coverage|Census|Total|Remarks|Acknowledgments)):')
+with open(os.path.join(data_path, "bbc_combined_1990.txt")) as infile:
+    lines = infile.read()
+    splitlines = lines.split("\n\n") # Break into one line per major chunk of data
+    p.split(splitlines[5])
