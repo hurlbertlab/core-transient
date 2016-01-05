ac53d2fb9bf9f1ad8a282efe440d3b623b492052
Sara <ssnell6@gmail.com>
2016-01-05 09:22:47 -0500

    final formatted script for d279

--- scripts/R-scripts/data_cleaning_scripts/dwork_279_SJS.R
+++ scripts/R-scripts/data_cleaning_scripts/dwork_279_SJS.R
@@ -154,7 +154,7 @@ dataFormattingTable[,'LatLong_sites'] =
   dataFormattingTableFieldUpdate(datasetID, 'LatLong_sites',  
                                  
                                  #--! PROVIDE INFO !--#
-                                 'Y') 
+                                 'N') 
 
 
 #-------------------------------------------------------------------------------*
@@ -270,7 +270,7 @@ dataFormattingTable[,'subannualTgrain'] =
 # fill in the fields that specify nested spatial grains below.
 
 #--! PROVIDE INFO !--#
-site_grain_names = c("station", "lakeid", "towdepth")
+site_grain_names = c("lakeid", "station", "towdepth")
 
 # We will now create the site field with these codes concatenated if there
 # are multiple grain fields. Otherwise, site will just be the single grain field.
