7d96747f12daa7314966302109d4706d58ac0766
Sara <ssnell6@gmail.com>
2016-02-12 11:22:19 -0500

    working on biotic interactions, beginning to test lms

--- scripts/R-scripts/data_cleaning_scripts/Biotic Interactions/Biotic interactions script.R
+++ scripts/R-scripts/data_cleaning_scripts/Biotic Interactions/Biotic interactions script.R
@@ -1,5 +1,5 @@
 #Biotic Interactions script
-
+library(plyr)
 #reading in dataset 
 Hurlbert_o = read.csv('scripts/R-scripts/data_cleaning_scripts/Biotic Interactions/Master_RO_Correlates_20110610.csv', header = T)
 #subsetting species whose occupancies were between 0.3 and 0.6 over a 10 eyar period
@@ -29,12 +29,23 @@ ep = read.csv('scripts/R-scripts/data_cleaning_scripts/Biotic Interactions/expec
 gt_ep = ep[ep$AOU == 5900,] 
 #merge expected occupancy w real occupancy
 obs_exp = merge(gt_ep, t1, by = "stateroute")
+#dropping duplicate columns
+drops <- c("SSTATENUMB","SROUTE", "AOU", "Year", "Aou")
+obs_exp = obs_exp[, !(names(obs_exp) %in% drops)]
+#renaming columns for clarity
+names(obs_exp) = c("stateroute", "spottedtotal", "GToccupancy")
+#aggregate based on year to get an average for each route
+obs_exp_2 = aggregate(obs_exp, by = list(obs_exp$stateroute), FUN = mean)
+#double checked with subset: mean(obs_exp$coyle_o.X5900[obs_exp$stateroute == 14168])
+#remove duplicate column
+drops <- c("Group.1")
+obs_exp_2 = obs_exp_2[, !(names(obs_exp_2) %in% drops)]
 #view where coyle_occupancy = 0 but predicted presence
-GT_gaps = obs_exp[obs_exp$coyle_o.X5900 == 0,] 
+GT_gaps = obs_exp_2[obs_exp_2$GToccupancy == 0,] 
 
 #merge in lat/long
 latlongs = read.csv('scripts/R-scripts/data_cleaning_scripts/Biotic Interactions/routes 1996-2010 consecutive.csv', header = T)
-plotdata_all = merge(obs_exp, latlongs, by = "stateroute") #where expected didnt equal observed
+plotdata_all = merge(obs_exp_2, latlongs, by = "stateroute") #where expected didnt equal observed
 plotdata = merge(GT_gaps, latlongs, by = "stateroute") #where expected didnt equal observed GT only
 #spotted range in point format
 spotty = merge(bbs, latlongs, by = "stateroute")
@@ -51,10 +62,26 @@ points(plotdata$Longi, plotdata$Lati, col = 4, pch = 17)
 #occupancy on GT, abundance on spotteed
 #LT averge for GT towhee for 10 year
 
+#read in env data 
+#from biol 465 final project, Snell Project Final.R script
+env = read.csv('scripts/R-scripts/data_cleaning_scripts/Biotic Interactions/BIOL465_final.csv', header = T)
+#merge env data w obs_exp
+env_occu_matrix = merge(env, bbs, by.x = "Species", by.y = "Aou")
+#aggregate based on year to get an average for each route
+#t = aggregate(env_occu_matrix, by = list(env_occu_matrix$stateroute), FUN = mean)
+#merge env matrix w occupancy for towhee
+finalmodelmatrix = merge(obs_exp_2, env_occu_matrix, by = "stateroute", all = FALSE)
 #starting lm analysis
-f1 = lm(stateroute ~ coyle_o.X5900 * Year, data = obs_exp)
+f1 = lm(GToccupancy ~  stateroute, data = finalmodelmatrix)
 summary(f1)
 
+f2 = lm(GToccupancy ~  stateroute*spottedtotal, data = finalmodelmatrix)
+summary(f2)
+
+f3 = lm(GToccupancy ~  stateroute*spottedtotal, data = finalmodelmatrix)
+  
+  
+  
 #NOTES
 #flycathcers, green tailed tohee
 #most likely asymmetric interactions - one sp will be dominant
