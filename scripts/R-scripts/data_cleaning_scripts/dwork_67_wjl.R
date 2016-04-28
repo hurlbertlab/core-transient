34abb4c66d79382493cd182ecd1a2ca6fb63357f
Allen Hurlbert <hurlbert@bio.unc.edu>
2016-04-28 14:43:21 -0400

    updated cleaning script and data formatting table for d67

--- scripts/R-scripts/data_cleaning_scripts/dwork_67_wjl.R
+++ scripts/R-scripts/data_cleaning_scripts/dwork_67_wjl.R
@@ -224,6 +224,22 @@ site = str_sub(site, start = 14)
 unique(site)
 length(site)
 
+# What is the spatial grain of the finest sampling scale? 
+# For this dataset spatial grain varies considerably and we don't have the means currently to extract it.
+
+dataFormattingTable[,'Raw_spatial_grain'] = 
+  dataFormattingTableFieldUpdate(datasetID, 'Raw_spatial_grain',  
+                                 
+                                 #--! PROVIDE INFO !--#
+                                 'NA') 
+
+dataFormattingTable[,'Raw_spatial_grain_unit'] = 
+  dataFormattingTableFieldUpdate(datasetID, 'Raw_spatial_grain',  
+                                 
+                                 #--! PROVIDE INFO !--#
+                                 'NA') 
+
+
 # BEFORE YOU CONTINUE. We need to make sure that there are at least minNTime for sites at the coarsest possilbe spatial grain. 
 
 siteCoarse = dataset2[, site_grain_names[1]]
@@ -289,8 +305,7 @@ dataFormattingTable[,'spatial_scale_variable'] =
 dataFormattingTable[,'Notes_siteFormat'] = 
   dataFormattingTableFieldUpdate(datasetID, 'Notes_siteFormat',  # Fill value below in quotes
                                  
-                                 'Sites are listed as lat_long, but each lat_long corresponds to a specific wetland. 
-                                 Thus we are NOT treating this like a lat-long dataset that could be spatially aggregated.')
+                                 'Sites are listed as lat_long, but each lat_long corresponds to a specific wetland. Thus we are NOT treating this like a lat-long dataset that could be spatially aggregated.')
 
 
 #-------------------------------------------------------------------------------*
@@ -359,9 +374,7 @@ dataFormattingTable[,'countFormat'] =
 dataFormattingTable[,'Notes_countFormat'] = 
   dataFormattingTableFieldUpdate(datasetID, 'Notes_countFormat', 
                                  
-                                 "Count data are suspected to be sums of binary presence data across surveys
-                                 within any one year. They should not be relied on for estimates of abundance
-                                 or mean abundance.")
+                                 "Count data are suspected to be sums of binary presence data across surveys within any one year. They should not be relied on for estimates of abundance or mean abundance.")
 
 #-------------------------------------------------------------------------------*
 # ---- EXPLORE AND FORMAT SPECIES DATA ----
@@ -516,7 +529,7 @@ write.csv(dataset7, paste("data/formatted_datasets/dataset_", datasetID, ".csv",
 
 # !GIT-ADD-COMMIT-PUSH THE FORMATTED DATASET IN THE DATA FILE, THEN GIT-ADD-COMMIT-PUSH THE UPDATED DATA FOLDER!
 
-# As we've now successfully created the formatted dataset, we will now update the format priority and format flag fields. 
+# As we've now successfully created the formatted dataset, we will now update the format flag field. 
 
 dataFormattingTable[,'format_flag'] = 
   dataFormattingTableFieldUpdate(datasetID, 'format_flag',    # Fill value below
@@ -589,7 +602,7 @@ site_grain_names
 sGrain = 'SampleID'
 
 # This is a reasonable choice of spatial grain because ...
-# ... rounding the latitude and longitude of each site to multiples of 2 creates blocks that are large enough to represent a bird community. 
+# ...each wetland was assumed to be of a scale relevant for observation. 
 
 # The function "richnessYearSubsetFun" below will subset the data to sites with an 
 # adequate number of years of sampling and species richness. If there are no 
@@ -610,6 +623,26 @@ head(richnessYearsTest)
 dim(richnessYearsTest) ; dim(dataset7)
 length(unique(richnessYearsTest$analysisSite))
 
+#Number of unique sites meeting criteria
+goodSites = unique(richnessYearsTest$analysisSite)
+length(goodSites)
+
+# Now subset dataset7 to just those goodSites as defined. This is tricky though
+# because assuming Sgrain is not the finest resolution, we will need to use
+# grep to match site names that begin with the string in goodSites.
+# The reason to do this is that sites which don't meet the criteria (e.g. not
+# enough years of data) may also have low sampling intensity that constrains
+# the subsampling level of the well sampled sites.
+
+uniqueSites = unique(dataset7$site)
+fullGoodSites = c()
+for (s in goodSites) {
+  tmp = as.character(uniqueSites[grepl(paste(s, "_", sep = ""), paste(uniqueSites, "_", sep = ""))])
+  fullGoodSites = c(fullGoodSites, tmp)
+}
+
+dataset8 = subset(dataset7, site %in% fullGoodSites)
+
 # Once we've settled on spatial and temporal grains that pass our test above,
 # we then need to 1) figure out what levels of spatial and temporal subsampling
 # we should use to characterize that analysis grain, and 2) subset the
@@ -624,7 +657,7 @@ length(unique(richnessYearsTest$analysisSite))
 # and bases the characterization of the community in that site-year based on
 # the aggregate of those standardized subsamples.
 
-subsettedData = subsetDataFun(dataset7, datasetID, spatialGrain = sGrain, 
+subsettedData = subsetDataFun(dataset8, datasetID, spatialGrain = sGrain, 
                               temporalGrain = tGrain,
                               minNTime = minNTime, minSpRich = minSpRich,
                               proportionalThreshold = topFractionSites,
@@ -647,6 +680,15 @@ writePropOccSiteSummary(subsettedData)
 # REMOVE meanAbundance info which is unreliable (see notes above)
 summ$meanAbundance = NA
 write.csv(summ, 'data/siteSummaries/siteSummary_67.csv', row.names = F)
+
+# Update Data Formatting Table with summary stats of the formatted,
+# properly subsetted dataset
+dataFormattingTable = dataFormattingTableUpdateFinished(datasetID, subsettedData)
+
+# And write the final data formatting table:
+
+write.csv(dataFormattingTable, 'data_formatting_table.csv', row.names = F)
+
 
 # Remove all objects except for functions from the environment:
 
