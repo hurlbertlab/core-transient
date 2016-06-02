065854cd941d0720c43e50c49de872d367217000
Allen Hurlbert <hurlbert@bio.unc.edu>
2016-06-02 15:57:49 -0400

    updated cleaning script an data formatting table for d263

--- scripts/R-scripts/data_cleaning_scripts/dwork_263_sjs.R
+++ scripts/R-scripts/data_cleaning_scripts/dwork_263_sjs.R
@@ -1,29 +1,9 @@
 ################################################################################*
-#  DATA FORMATTING TEMPLATE
-################################################################################*
 #
 # Dataset name: Species Turnover Rates on Islands: Dependence on Census Interval
 # Dataset source (link): http://www.jstor.org/stable/1744518?seq=1#page_scan_tab_contents
 # Formatted by: Sara Snell
 #
-# Start by opening the data formatting table (data_formatting_table.csv). 
-# Datasets to be worked on will have a 'format_flag' of 0.
-
-# Flag codes are as follows:
-  # 0 = not currently worked on
-  # 1 = formatting complete
-  # 2 = formatting in process
-  # 3 = formatting halted, issue
-  # 4 = data unavailable
-  # 5 = data insufficient for generating occupancy data
-
-# NOTE: All changes to the data formatting table will be done in R! 
-# Do not make changes directly to this table, this will create conflicting versions.
-
-# YOU WILL NEED TO ENTER DATASET-SPECIFIC INFO IN EVERY LINE OF CODE PRECEDED
-# BY "#--! PROVIDE INFO !--#". 
-
-# YOU SHOULD RUN, BUT NOT OTHERWISE MODIFY, ALL OTHER LINES OF CODE.
 
 #-------------------------------------------------------------------------------*
 # ---- SET-UP ----
@@ -74,7 +54,7 @@ dataFormattingTable[,'Raw_datafile_name'] =
   dataFormattingTableFieldUpdate(datasetID, 'Raw_datafile_name',  
                                  
 #--! PROVIDE INFO !--#
-  '1744518.pdf') 
+  'extracted from Diamond and May 1977') 
 
 
 
