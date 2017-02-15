00e04c07cd4f1f0318a8c953e93b4edc5325f51f
Molly Jenkins <mollyfrn@live.unc.edu>
2017-02-15 17:42:46 -0500

    fixed log to log10; fixed plotting and rewrote plot pdfs

--- scripts/R-scripts/scale_analysis/alt_occ_scale.R
+++ scripts/R-scripts/scale_analysis/alt_occ_scale.R
@@ -212,13 +212,16 @@ bbs_allscales = rbind(bbs_below, bbs_focal_occs)
 
 ####Cross-scale analysis and visualization####
 bbs_allscales = read.csv("data/bbs_allscales.csv", header = TRUE)
+bbs_allscales$logA = log10(bbs_allscales$area)
+bbs_allscales$logN = log10(bbs_allscales$aveN)
 
-mod1 = lm(meanOcc~area, data = bbs_allscales) #explains ~50% of the variation in occ
-mod2 = lm(meanOcc~aveN, data = bbs_allscales)
+
+mod1 = lm(meanOcc~logA, data = bbs_allscales) #explains ~50% of the variation in occ
+mod2 = lm(meanOcc~logN, data = bbs_allscales)
 summary(mod1)
 
-plot(meanOcc~log(area), data = bbs_allscales, xlab = "Log Area" , ylab = "Mean Temporal Occupancy")
-plot(meanOcc~aveN, data = bbs_allscales, xlab = "Average Abundance" , ylab = "Mean Temporal Occupancy")
+plot(meanOcc~logA, data = bbs_allscales, xlab = "Log Area" , ylab = "Mean Temporal Occupancy")
+plot(meanOcc~logN, data = bbs_allscales, xlab = "Average Abundance" , ylab = "Mean Temporal Occupancy")
 #^^same pattern
 
 
@@ -235,14 +238,14 @@ for (s in stateroutes) {
 #log(area)
 theme_set(theme_bw()+theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()))
 plotsub = subset(bbs_allscales, bbs_allscales$focalrte == s)
-plot1 = ggplot(plotsub, aes(x = log(area), y = meanOcc))+geom_point(color = "firebrick")+geom_point()+geom_smooth(se=FALSE)
-plot1_2= ggplot(plotsub, aes(x = log(area), y = pctCore))+geom_point(color = "turquoise")+geom_point()+geom_smooth(se=FALSE)
-plot1_3 = ggplot(plotsub, aes(x = log(area), y = pctTran))+geom_point(color = "olivedrab")+geom_point()+geom_smooth(se=FALSE)
+plot1 = ggplot(plotsub, aes(x = logA, y = meanOcc))+geom_point(colour = "firebrick")+geom_smooth(se=FALSE)
+plot1_2= ggplot(plotsub, aes(x = logA, y = pctCore))+geom_point(colour = "turquoise")+geom_smooth(se=FALSE)
+plot1_3 = ggplot(plotsub, aes(x = logA, y = pctTran))+geom_point(colour = "olivedrab")+geom_smooth(se=FALSE)
 
 #aveN
-plot2 = ggplot(plotsub, aes(x=log(aveN), y =meanOcc))+geom_point(color = "firebrick")+geom_point()+geom_smooth(se=FALSE)
-plot2_2 = ggplot(plotsub, aes(x=log(aveN), y =pctCore))+geom_point(color = "turquoise")+geom_point()+geom_smooth(se=FALSE)
-plot2_3 =ggplot(plotsub, aes(x=log(aveN), y =pctTran))+geom_point(color = "olivedrab")+geom_point()+geom_smooth(se=FALSE)
+plot2 = ggplot(plotsub, aes(x=logN, y =meanOcc))+geom_point(colour = "firebrick")+geom_smooth(se=FALSE)
+plot2_2 = ggplot(plotsub, aes(x=logN, y =pctCore))+geom_point(colour = "turquoise")+geom_smooth(se=FALSE)
+plot2_3 =ggplot(plotsub, aes(x=logN, y =pctTran))+geom_point(colour = "olivedrab")+geom_smooth(se=FALSE)
 
 
 ####change to log_10^^^^####
@@ -278,7 +281,7 @@ for(s in stateroutes){
   logsub = subset(bbs_allscales, bbs_allscales$focalrte == s)  
   #fitting the log curve for area (for each route)
     OAmodel = tryCatch({
-    OAlog = nls(meanOcc ~ SSlogis(log(area), Asym, xmid, scal), data = logsub)
+    OAlog = nls(meanOcc ~ SSlogis(logA, Asym, xmid, scal), data = logsub)
     return(data.frame(stateroute = s, OA.A, OA.i, OA.k))
   }, warning = function(w) {
     warnings = rbind(warnings, data.frame(stateroute = s, warning = w))
@@ -302,7 +305,7 @@ for(s in stateroutes){
   logsub = subset(bbs_allscales, bbs_allscales$focalrte == s)  
   #fitting the log curve for aveN (for each route)
   ONmodel = tryCatch({
-    ONlog = nls(meanOcc ~ SSlogis(log(aveN), Asym, xmid, scal), data = logsub)
+    ONlog = nls(meanOcc ~ SSlogis(logN, Asym, xmid, scal), data = logsub)
     return(data.frame(stateroute = s, ON.A, ON.i, ON.k))
   }, warning = function(w) {
     warnings = rbind(warnings, data.frame(stateroute = s, warning = w))
@@ -326,7 +329,7 @@ for(s in stateroutes){
   logsub = subset(bbs_allscales, bbs_allscales$focalrte == s)  
   #fitting the log curve for area (for each route)
   CAmodel = tryCatch({
-    CAlog = nls(pctCore ~ SSlogis(log(area), Asym, xmid, scal), data = logsub)
+    CAlog = nls(pctCore ~ SSlogis(logA, Asym, xmid, scal), data = logsub)
     return(data.frame(stateroute = s, CA.A, CA.i, CA.k))
   }, warning = function(w) {
     warnings = rbind(warnings, data.frame(stateroute = s, warning = w))
@@ -350,7 +353,7 @@ for(s in stateroutes){
   logsub = subset(bbs_allscales, bbs_allscales$focalrte == s)  
   #fitting the log curve for aveN (for each route)
   CNmodel = tryCatch({
-    CNlog = nls(pctCore ~ SSlogis(log(aveN), Asym, xmid, scal), data = logsub)
+    CNlog = nls(pctCore ~ SSlogis(logN, Asym, xmid, scal), data = logsub)
     return(data.frame(stateroute = s, CN.A, CN.i, CN.k))
   }, warning = function(w) {
     warnings = rbind(warnings, data.frame(stateroute = s, warning = w))
@@ -380,19 +383,19 @@ for(s in stateroutes){
 #revised ref:
 
 pT.0 <- min(logsub$pctTran) * 0.5
-model.0 <- lm(log(pctTran - pT.0) ~ log(area), data=logsub)
+model.0 <- lm(log(pctTran - pT.0) ~ logA, data=logsub)
 start <- list(Asym=exp(coef(model.0)[1]), xmid=coef(model.0)[2], scal=pT.0)
-TAlog = nls(pctTran ~ Asym/(1 + exp((xmid - log(area))/scal)),   #Asym is a, xmid is b, and scal is c with pctTran as cost and area as redux
+TAlog = nls(pctTran ~ Asym/(1 + exp((xmid - logA)/scal)),   #Asym is a, xmid is b, and scal is c with pctTran as cost and area as redux
             data = logsub, 
             start = start)
 #^^^above code still produces singular gradient matrix even though it's supposed to solve that exact problem
 
 
-TAlog = nls(pctTran ~ Asym/(1 + exp((xmid - log(area))/scal)),  
+TAlog = nls(pctTran ~ Asym/(1 + exp((xmid - logA)/scal)),  
             start = list(xmid = 0.4, scal = -0.5, Asym = 0.08), data = logsub)
 #model written out
 
-TAlog = nls(pctTran ~ SSlogis(log(area), Asym, xmid, scal), 
+TAlog = nls(pctTran ~ SSlogis(logA, Asym, xmid, scal), 
             data = logsub, start = list(xmid = -0.4, scal = 0.5, Asym = 0.08))
 #self starting 
 
@@ -406,7 +409,7 @@ TAlog = nls(pctTran ~ SSlogis(log(area), Asym, xmid, scal),
 #   logsub = subset(bbs_allscales, bbs_allscales$focalrte == s)
 #   #fitting the log curve for area (for each route)
 #   TAmodel = tryCatch({
-#     TAlog = nls(pctTran ~ Asym/(1 + exp((xmid - log(area))/scal)),  
+#     TAlog = nls(pctTran ~ Asym/(1 + exp((xmid - logA)/scal)),  
 #               start = list(xmid = 1, scal = 1, Asym = 0.1), data = logsub) #this code produces singular gradient matrix error 
 #     return(data.frame(stateroute = s, TA.A, TA.i, TA.k))
 #   }, warning = function(w) {
@@ -431,7 +434,7 @@ TAlog = nls(pctTran ~ SSlogis(log(area), Asym, xmid, scal),
 #   logsub = subset(bbs_allscales, bbs_allscales$focalrte == s)  
 #   #fitting the log curve for aveN (for each route)
 #   TNmodel = tryCatch({
-#     TNlog = nls(pctTran ~ SSlogis(log(aveN), Asym, xmid, scal), data = logsub)
+#     TNlog = nls(pctTran ~ SSlogis(logN, Asym, xmid, scal), data = logsub)
 #     return(data.frame(stateroute = s, TN.A, TN.i, TN.k))
 #   }, warning = function(w) {
 #     warnings = rbind(warnings, data.frame(stateroute = s, warning = w))
@@ -464,7 +467,7 @@ plot1 + geom_point() +    # Use hollow circles
 #extracted coefs for analysis; but plotting is auto 
 #is loess ok?
 
-curvemod = nls(meanOcc ~ A/(1+exp(i - log(area))/k), 
+curvemod = nls(meanOcc ~ A/(1+exp(i - logA)/k), 
                data = plotsub) 
 summary(curvemod)
 
