2e812847b05fad0081d2df3b8634d81bced86749
Snell <ssnell@ad.unc.edu>
2017-03-02 10:31:30 -0500

    created species are curve 4c - need to add non count data back in

--- scripts/R-scripts/summary_and_analysis.r
+++ scripts/R-scripts/summary_and_analysis.r
@@ -527,25 +527,34 @@ scaleIDs = filter(dataformattingtable, spatial_scale_variable == 'Y',
                   format_flag == 1)$dataset_ID 
 scaleIDs = scaleIDs[! scaleIDs %in% c(207, 210, 217, 218, 222, 223, 225, 238, 241,258, 282, 322, 280,317, 248, 221, 313)]  # waiting on data for 248
 
+scaleIDs = scaleIDs[! scaleIDs %in% c(254, 291)]
+
+slopes = c()
 for(id in scaleIDs){
   print(id)
-  plotsub = subset(bbs_occ_trans_area,datasetID == id)          ###### need to do this by trans and no trans - 2 DFs maybe?
-  mod.t = lm(log10(plotsub$meanAbundance) ~ log10(plotsub$area))
-  mod.n= lm(log10(plotsub$meanAbundance) ~ log10(plotsub$area))
-  xnew = range(plotsub$minustrans)
-  xhat <- predict(mod3, newdata = data.frame((xnew)))
-  xhats = range(xhat)
-  print(xhats)
+  plotsub = subset(bbs_occ_trans_area,datasetID == id) 
+  taxa = as.character(unique(plotsub$taxa))
+  mod.t = lm(log10(plotsub$spRich) ~ log10(plotsub$area))
+  mod.t.slope = summary(mod.t)$coef[2,"Estimate"]
+  mod.n= lm(log10(plotsub$minustrans) ~ log10(plotsub$area))
+  mod.n.slope = summary(mod.n)$coef[2,"Estimate"]
   taxcolor = subset(taxcolors, taxa == as.character(plotsub$taxa)[1])
   y=summary(mod3)$coef[1] + (xhats)*summary(mod3)$coef[2]
-  plot(NA, xlim = c(-1, 30), ylim = c(0,30), col = as.character(taxcolor$color), xlab = expression("Transients"), ylab = "No Transients", cex = 1.5)
-  lines(plotsub$minustrans, fitted(mod3), col=as.character(taxcolor$color),lwd=5)
-  par(new=TRUE)
+  slopes = rbind(slopes, c(mod.t.slope, mod.n.slope, taxa))
 }
-par(new=TRUE)
-legend('bottomleft', legend = as.character(taxcolors$taxa), lty=1,lwd=3,col = as.character(taxcolors$color), cex = 1)
-dev.off()
+colnames(slopes) = c("spRich_slope","minustrans_slope", "taxa")
+plot_relationship = merge(slopes, taxcolors, by = "taxa")
+plot_relationship$spRich_slope = as.numeric(plot_relationship$spRich_slope)
+plot_relationship$minustrans_slope = as.numeric(plot_relationship$minustrans_slope)
 
+
+plot_relationship$taxa = factor(plot_relationship$taxa,
+                        levels = c('Invertebrate','Fish','Plankton','Mammal','Plant','Bird','Benthos'),ordered = TRUE)
+colscale = c("gold2","turquoise2", "red", "purple4","forestgreen","#1D6A9B", "azure4")
+
+p <- ggplot(plot_relationship, aes(x = spRich_slope, y = minustrans_slope))
+p + geom_abline(intercept = 0,slope = 1, lwd =1.5,linetype="dashed")+geom_point(aes(colour = taxa), size = 4) + xlab("Species Richness") + ylab("Species Richness Without Transients") + scale_colour_manual(breaks = plot_relationship$taxa,values = colscale) + theme(axis.text.x=element_text(size=24),axis.text.y=element_text(size=24),axis.title.x=element_text(size=32),axis.title.y=element_text(size=32,angle=90,vjust = 2))+ theme_classic()
+ggsave(file="C:/Git/core-transient/output/plots/sparea_4c.pdf", height = 10, width = 15)
 ####### MODELS ######
 latlongs = read.csv("data/latlongs/latlongs.csv", header =TRUE)
 
