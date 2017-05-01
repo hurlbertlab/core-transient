4076fd571ffed1669e49caba6e63e264e036df3f
Molly Jenkins <mollyfrn@live.unc.edu>
2017-05-01 16:01:31 -0400

    sep out env analysis portion

--- scripts/R-scripts/scale_analysis/env_analysis.R
+++ scripts/R-scripts/scale_analysis/env_analysis.R
@@ -66,3 +66,144 @@ bbs_envs = bbs_latlon
 #wrote file 03/28 w/elev from getData and using old env data; need to overwrite with updated env vars
 
 #merge into bbs_allscales data
+
+
+####Env data add-in: Alternate 'cookie-cutter' method for env variables####
+#based on Sara's code from summary_and_analysis.R file
+
+# Makes routes into a spatialPointsDataframe
+coordinates(all_latlongs)=c('Lon','Lat')
+projection(all_latlongs) = CRS("+proj=longlat +ellps=WGS84")
+prj.string <- "+proj=laea +lat_0=45.235 +lon_0=-106.675 +units=km"
+# Transforms routes to an equal-area projection - see previously defined prj.string
+routes.laea = spTransform(all_latlongs, CRS(prj.string))
+
+##### extracting elevation data ####
+# A function that draws a circle of radius r around a point: p (x,y)
+RADIUS = 40
+
+make.cir = function(p,r){
+  points=c()
+  for(i in 1:360){
+    theta = i*2*pi/360
+    y = p[2] + r*cos(theta)
+    x = p[1] + r*sin(theta)
+    points = rbind(points,c(x,y))
+  }
+  points=rbind(points,points[1,])
+  circle=Polygon(points,hole=F)
+  circle
+}
+
+routes.laea@data$dId_site = paste(routes.laea@data$datasetID, routes.laea@data$site, sep = "_")
+routes.laea@data$unique = 1:16602
+
+
+#Draw circles around all routes 
+circs = sapply(1:nrow(routes.laea@data), function(x){
+  circ =  make.cir(routes.laea@coords[x,],RADIUS)
+  circ = Polygons(list(circ),ID=routes.laea$unique[x]) 
+}
+)
+
+circs.sp = SpatialPolygons(circs, proj4string=CRS(prj.string))
+
+# Check that circle locations look right
+plot(circs.sp)
+
+# read in elevation raster at 1 km resolution
+elev <- raster("Z:/GIS/DEM/sdat_10003_1_20170424_102000103.tif")
+NorthAm = readOGR("Z:/GIS/geography", "continent")
+
+plot(elev)
+plot(NorthAm,add=TRUE)
+
+elevNA <- raster::mask(elev, NorthAm)
+
+
+elev.point = raster::extract(elevNA, routes.laea)
+elev.mean = raster::extract(elevNA, circs.sp, fun = mean, na.rm=T)
+elev.var = raster::extract(elevNA, circs.sp, fun = var, na.rm=T)
+
+env_elev = data.frame(unique = routes.laea@data$unique, elev.point = elev.point, elev.mean = elev.mean, elev.var = elev.var)
+
+
+lat_scale_elev = merge(routes.laea, env_elev, by = c("unique")) # checked to make sure order lined up, d/n seem to be another way to merge since DID keeps getting lost
+lat_scale_elev = data.frame(lat_scale_elev)
+
+####Coef vs env variation models####
+bbs_envs = read.csv("scripts/R-scripts/scale_analysis/bbs_envs.csv", header = TRUE)
+coefs = read.csv("scripts/R-scripts/scale_analysis/coefs.csv", header = TRUE)
+uniq_env = unique(bbs_envs[, c('focalrte', 'temp', 'vartemp', 'meanP', 'varP', 'ndvi', 'varndvi', 'elev')])
+env_coefs = inner_join(coefs, uniq_env, by = c('stateroute' = 'focalrte'))
+covmatrix = round(cor(coefs[, 2:ncol(coefs)]), 2)
+
+
+# nested loop for examining variation in coefs/fitted curves explained by env vars 
+rsqrd_df = data.frame(dep = character(), ind = character(), r2 = numeric())
+
+for (d in 2:25) {
+  for (i in 26:ncol(env_coefs)) {
+    tempmod = lm(env_coefs[,d] ~ env_coefs[,i])
+    tempdf = data.frame(dep = names(env_coefs)[d], 
+                        ind = names(env_coefs)[i], 
+                        r2 = summary(tempmod)$r.squared)
+    rsqrd_df = rbind(rsqrd_df, tempdf)
+  }
+}
+#write.csv(rsqrd_df, "scripts/R-scripts/scale_analysis/mod_rsqrds.csv", row.names = FALSE) #updated 03/28 with elev
+
+
+####Visually Characterizing r2 vals####
+rsqrd_df = read.csv("scripts/R-scripts/scale_analysis/mod_rsqrds.csv", header = TRUE)
+
+ggplot(data = rsqrd_df, aes(x = ind, y = r2, fill = ind))+geom_boxplot()+theme_classic()+
+  scale_fill_manual(values = wes_palette("BottleRocket"))+theme(legend.position="none")+
+  labs(x = "Environmental variables", y = "Variation Explained (R^2)")
+
+#excluding transient data for incompleteness
+rsub_i = rsqrd_df %>%
+  filter(dep == "OA.i" | dep == "ON.i" | dep == "CA.i" | dep == "CN.i") %>%
+  filter(ind == "elev" | ind == "meanP" | ind == "ndvi" | ind == "temp")
+rsub_i = droplevels(rsub_i) #removing ghost levels to ensure correct plotting/analyses
+
+ggplot(data = rsub_i, aes(x = ind, y = r2)) + geom_boxplot()+theme_classic() #what I used for poster w/out color 
+
+
+#separate analysis for just transients since relationship not immediately apparent
+rsub_t = rsqrd_df %>%
+  filter(dep == "TAexp" | dep == "TApow" | dep == "TNexp" | dep == "TNpow") %>%
+  filter(ind == "elev" | ind == "meanP" | ind == "ndvi" | ind == "temp")
+rsub_t = droplevels(rsub_t) #removing ghost levels to ensure correct plotting/analyses
+
+ggplot(data = rsub_t, aes(x = ind, y = r2)) + geom_boxplot()+theme_classic() #elev explains more variation in the transients
+
+####Variance Partitioning of Env Predictors####
+#would I be basing my total remaining unexplained variation off of the meanOcc~logA relationship? (OA.i?)
+#so the 12% remaining
+#focusing just on OA.i and main env vars
+#how do variance partitioning with more than 4 parts? 
+
+globalmod<-lm(OA.i~elev+meanP+temp+ndvi, data=env_coefs)
+mod1<-lm(OA.i~elev, data=env_coefs)
+mod2<-lm(OA.i~meanP, data=env_coefs)
+mod3<-lm(OA.i~ndvi, data=env_coefs)
+mod4<-lm(OA.i~temp, data=env_coefs)
+#and then Euclid_mod2
+summary(globalmod)$r.squared
+summary(mod1)$r.squared
+summary(mod2)$r.squared
+summary(mod3)$r.squared
+summary(mod4)$r.squared 
+
+
+#running with mods 2+3 bc best ranked and most interesting 
+a= summary(globalmod)$r.squared - summary(mod2)$r.squared
+a
+c= summary(globalmod)$r.squared - summary(mod3)$r.squared
+c
+b= summary(mod2)$r.squared - c
+b
+d= 1- summary(globalmod)$r.squared
+d
+#isn't it ok that d = ~87.5% tho, given that the r^2 for occ~logA was 88%? 
