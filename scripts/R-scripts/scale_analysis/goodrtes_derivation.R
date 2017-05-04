48c18bfa5b3bc54213c6c0a978f4fa02c8d832ad
Molly Jenkins <mollyfrn@live.unc.edu>
2017-05-04 14:15:06 -0400

    double checked, clean, ran, re-saved good_rtes2 file

--- scripts/R-scripts/scale_analysis/goodrtes_derivation.R
+++ scripts/R-scripts/scale_analysis/goodrtes_derivation.R
@@ -5,12 +5,8 @@
 #I set this portion of the data cleaning into its own script so that it would be easier to keep track of 
 #where goodrtes etc. were being derived from, as well as which dataset corresponded to 50 vs non-50 stop 
 
-
-
-
-
 #Set working directory to core-transient folder on github i.e. setwd("C:/git/core-transient/")
-
+#years are 2000-2014 because 2001-2015 only provides 953 rtes; vs 2000-2014 provides 1005 routes
 
 ## Please download and install the following packages:
 # maps, sp, rgdal, raster, maptools, rgeos, dplyr, fields
@@ -22,23 +18,26 @@ library(maptools)
 library(rgeos)
 library(dplyr)
 library(fields)
+library(ecoretriever)
 
 ####Bringing in BBS50 stop data and prepping it for sub-route scale partitioning####
+# bbs50 = ecoretriever::fetch('BBS50')
+# bbs50 = bbs50$counts
+# bbs50$stateroute = bbs50$statenum*1000 + bbs50$Route
+# bbs50$stateroute = as.integer(bbs50$stateroute)
+#^derivation of data from ecoretriever; saved in bioark 
+bbs50 = read.csv("//bioark.ad.unc.edu/HurlbertLab/Jenkins/BBS scaled/bbs50.csv", header = TRUE) #includes 2015
 
-bbs50 = ecoretriever::fetch('BBS50')
-bbs50 = bbs50$counts
-bbs50$stateroute = bbs50$statenum*1000 + bbs50$Route
-bbs50$stateroute = as.integer(bbs50$stateroute)
-#^derivation of data from ecoretriever; still too large to host on github so save and pull from BioArk
+routes = read.csv("C:/git/core-transient/scripts/R-scripts/scale_analysis/routes.csv", header = TRUE)
+#file of ALL BBS routes for N. Am. with Lat Longs
+routes$stateroute = routes$statenum*1000 + routes$Route
 
-bbs50 = read.csv("//bioark.ad.unc.edu/HurlbertLab/Gartland/BBS scaled/bbs50.csv", header = TRUE)
 
-# Get subset of BBS routes (just routes) btw 1996-2010 surveyed in EVERY year
-
+# Get subset of BBS routes (just routes) btw 2000-2014 surveyed in EVERY year
 require(dplyr)
 #from Sara's code
 good_rtes = bbs50 %>% 
-  filter(year >= 2001, year <= 2015) %>% #shifted 15 year window up because missing 1996 data, and 2015 data available
+  filter(year >= 2000, year < 2015) %>% #shifted 15 year window to 200-2014 bc that's where 1005 happens
   select(year, stateroute) %>%
   unique() %>%    
   group_by(stateroute) %>%  
@@ -50,19 +49,19 @@ good_rtes = bbs50 %>%
 require(dplyr)
 # Subset the full BBS dataset to the routes above but including associated data
 fifty_allyears = bbs50 %>% 
-  filter(year >= 2000, year <= 2014) %>% 
+  filter(year >= 2000, year < 2015) %>% 
   filter(stateroute %in% good_rtes$stateroute)
 
 #finally works because needed $ specification, 
 #can probably collapse into one line 
 #write.csv(fifty_allyears, "//bioark.ad.unc.edu/HurlbertLab/Gartland/BBS scaled/fifty_allyears.csv", row.names = FALSE)
-
 #wrote to file just in case 
 
-# merge lat longs from routes file to the list of "good" routes (2000-2014 present all years)
+# merge lat longs from routes file to the list of "good" routes (2001-2015 present all years)
 require(dplyr)
 good_rtes2 = good_rtes %>% 
   left_join(routes, good_rtes, by = "stateroute") %>%
   dplyr::select(stateroute, Lati, Longi)
-#write.csv(good_rtes2, "//bioark.ad.unc.edu/HurlbertLab/Gartland/BBS scaled/good_rtes2.csv", row.names = FALSE)
+#write.csv(good_rtes2, "//bioark.ad.unc.edu/HurlbertLab/Jenkins/BBS scaled/good_rtes2.csv", row.names = FALSE)
+#updated 05/04/2017
 
