227df34e84ab23e555fed3a4dc7a2500d5798cf0
Molly Jenkins <mollyfrn@live.unc.edu>
2017-06-27 11:04:27 -0400

    initializing new analysis presenting alternative to fitting logistic curves to occ-scale equation; instead fitting simple equation of line bet/min and max points and extracting parms from that line to describe relationship

---
+++ scripts/R-scripts/scale_analysis/occ_line_eq.R
@@ -0,0 +1,6 @@
+#Occupancy-scale analysis
+
+##feat. alternative to curve-fitting parameters: slope, intercept, and x value @ scale of 3 aggregated routes.
+# author: Molly F. Jenkins
+# date: 06/27/2017
+# 
