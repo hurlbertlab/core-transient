6638524ad306bb3069b4be7ebc9e3343389daf31
Molly Jenkins <mollyfrn@live.unc.edu>
2017-07-24 10:44:49 -0400

    all clear on the occ_line_eq case of the missing scale

--- scripts/R-scripts/scale_analysis/occ_line_eq.R
+++ scripts/R-scripts/scale_analysis/occ_line_eq.R
@@ -72,21 +72,25 @@ warnings = data.frame(stateroute = numeric(), warning = character())
 #read in data for processing
 bbs_allscales = read.csv("data/BBS/bbs_allscales.csv", header = TRUE)
 stateroutes = unique(bbs_allscales$focalrte) #this stuff is the same, looks normal ^
+unique(bbs_allscales$scale)
+levels(bbs_allscales$scale) #checking before running loop that all scales rep'd
+
 
 #make sure scale is leveled in order: 
 bbs_allscales$scale = factor(bbs_allscales$scale, 
-                             levels = c('5-1', '5-2', '5-3', '5-4', '5-5', '5-6', '5-7', '5-8', '5-9', '5-10',
-                                        '10-1', '10-2', '10-3', '10-4', '10-5', '25-1', '25-2', '50-1', 
-                                        '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12',
-                                        '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24',
-                                        '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36',
-                                        '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48',
-                                        '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59', '60',
-                                        '61', '62', '63', '64', '65', '66'), ordered=TRUE)
+                              levels = c('5-1', '5-2', '5-3', '5-4', '5-5', '5-6', '5-7', '5-8', '5-9', '5-10',
+                                         '10-1', '10-2', '10-3', '10-4', '10-5', '25-1', '25-2', '50-1', 
+                                         '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12',
+                                         '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24',
+                                         '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36',
+                                         '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48',
+                                         '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59', '60',
+                                         '61', '62', '63', '64', '65', '66'), ordered=TRUE)
 
 
 levels(bbs_allscales$scale)
-#NEED to do or it won't be in order
+unique(bbs_allscales$scale)
+#ALL clear
 
 #07/16 version of tryCatch
 for(s in stateroutes){
