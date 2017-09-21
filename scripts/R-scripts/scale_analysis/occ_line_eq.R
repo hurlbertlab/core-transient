957e39efe207a6e4ce3545cdd24063815c38b1ec
Molly Jenkins <mollyfrn@live.unc.edu>
2017-09-21 14:57:36 -0400

    checking to make sure all updated

--- scripts/R-scripts/scale_analysis/occ_line_eq.R
+++ scripts/R-scripts/scale_analysis/occ_line_eq.R
@@ -233,7 +233,7 @@ coefs = OA.df %>%
   inner_join(CA.df, OA.df, by = "stateroute") %>% 
   inner_join(TA.df, OA.df, by = "stateroute") 
  
-write.csv(coefs, "scripts/R-scripts/scale_analysis/coefs.csv", row.names = FALSE) #updated 09/20
+write.csv(coefs, "scripts/R-scripts/scale_analysis/coefs.csv", row.names = FALSE) #updated 09/21
 #exp mods have much better r2 vals for pctTran than power 
 #checked, working correctly 08/28, output not NA's but normal!
 
@@ -298,10 +298,10 @@ bbs_top_W = bbs_top %>%
 
 #plotting comparisons: 
 
-lndscpe_rtfreqE = ggplot(bbs_top_E, aes(x=meanOcc))+geom_histogram(bins = 30)+coord_cartesian(xlim = c(0, 0.95), ylim = c(0, 80))+labs(title = "Landscape East") 
+lndscpe_rtfreqE = ggplot(bbs_top_E, aes(x=meanOcc))+geom_histogram(bins = 30)+coord_cartesian(xlim = c(0.5, 0.95), ylim = c(0, 80))+labs(title = "Landscape East") 
 lndscpe_rtfreqE
 
-lndscpe_rtfreqW = ggplot(bbs_top_W, aes(x=meanOcc))+geom_histogram(bins = 30)+coord_cartesian(xlim = c(0, 0.95), ylim = c(0, 80))+labs(title = "Landscape West")  
+lndscpe_rtfreqW = ggplot(bbs_top_W, aes(x=meanOcc))+geom_histogram(bins = 30)+coord_cartesian(xlim = c(0.5, 0.95), ylim = c(0, 80))+labs(title = "Landscape West")  
 lndscpe_rtfreqW
 
 #WOW LOOK AT THOSE DISCREPANCIES!!!!
@@ -333,110 +333,3 @@ AUC_Eplot #mirrors the first dist
 AUC_Wplot = ggplot(AUC_W, aes(x = OA.AUC))+geom_histogram(bins = 30)
 AUC_Wplot
 #bigger spread, def not normal dist if we narrowed down
-
-
-###idk what I was trying to do here, revisit w/lab notebook later 
-coefs_mini = coefs %>%
-  select(stateroute, OA.pmin, OA.pmax) %>%
-  gather(key = OA.pmin, 
-         value = occ_val, 
-         OA.pmin:OA.pmax, 
-         na.rm = TRUE) %>% #doubled size of df, every rte has associated max and min vals 
-  rename(linemetric = OA.pmin)
-ggplot(coefs_mini, aes(x = linemetric, y = occ_val))+geom_boxplot(aes(fill = linemetric))+
-  theme_classic()+theme(legend.position="none")
-
-
-#will go inside plotting forloop 
-coef_join = coefs %>% inner_join(bbs_allscales, by = c("stateroute"="focalrte"))
-#stateroutes = unique(bbs_allscales$focalrte)
-s = 2001
-coef_sub = subset(coef_join, coef_join$stateroute == s)
-
-
-theme_set(theme_bw()+theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()))
-#OA
-OAlog = lm(meanOcc ~ logA, data = coef_sub) #lm instead of nls, reg linear model
-OApreds = data.frame(preds = predict(OAlog), scale = coef_sub$scale, logA = coef_sub$logA) 
-
-
-plot1 = ggplot(coef_join, aes(x = logA, y = meanOcc))+geom_point()+
-  geom_smooth(aes(colour = stateroute), intercept = OA.xmid, slope = OA.pslope) +labs(x = "Log area", y = "Mean % Occupancy")+
-  coord_cartesian(ylim = c(0, 1))
-
-
-ggplot(coef_join, aes(x = logA, y = meanOcc))+geom_point()+geom_point(aes(x = OA.min))
-#in order to plot min and maxes need x and y, just duplicating vals in both directions rn!
-
-
-####Old plotting code from logfcn####
-preds.df = data.frame(stateroute = numeric(), logA = numeric(), 
-                      OApreds= numeric(), ONpreds = numeric(), 
-                      CApreds = numeric(), CNpreds = numeric(),
-                      TApreds = numeric(), TNpreds = numeric())
-
-
-pdf("output/plots/Molly Plots/BBS_scaleplots.pdf", onefile = TRUE)
-coef_join = coefs %>% inner_join(bbs_allscales3, by = c("stateroute"="focalrte"))
-stateroutes = unique(bbs_allscales3$focalrte)
-
-#extracting predicted values and plotting in same loop
-for (s in stateroutes) {
-  theme_set(theme_bw()+theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()))
-  coef_sub = subset(coef_join, coef_join$stateroute == s)
-  logA = coef_sub$logA
-  
-  #OA
-  OApreds = logistic_fcn(coef_sub[,33], coef_sub[,2], coef_sub[,3], coef_sub[,4]) 
-  plot1 = ggplot(coef_sub, aes(x = logA, y = meanOcc))+geom_point(colour = "firebrick")+
-    geom_line(aes(x = logA, y = OApreds), color = "navy") +labs(x = "Log area", y = "Mean % Occupancy")+
-    coord_cartesian(ylim = c(0, 1))
-  
-  
-  #ON
-  ONpreds = logistic_fcn(coef_sub[,34], coef_sub[,6], coef_sub[,7], coef_sub[,8])
-  plot2 = ggplot(coef_sub, aes(x = logN, y = meanOcc))+geom_point(colour = "firebrick")+
-    geom_line(aes(x = logN, y = ONpreds), color = "navy") +labs(x = "Log abundance", y = "Mean % Occupancy")+
-    coord_cartesian(ylim = c(0, 1))
-  
-  
-  #CA
-  CApreds = logistic_fcn(coef_sub[,33], coef_sub[,10], coef_sub[,11], coef_sub[,12])
-  plot1_2= ggplot(coef_sub, aes(x = logA, y = pctCore))+geom_point(colour = "turquoise")+
-    geom_line(aes(x = logA, y = CApreds), color = "navy")+labs(x = "Log area", y = "% Core Occupancy")+
-    coord_cartesian(ylim = c(0, 1))
-  
-  #aveN
-  #CN
-  CNpreds = logistic_fcn(coef_sub[,34], coef_sub[,14], coef_sub[,15], coef_sub[,16])
-  plot2_2= ggplot(coef_sub, aes(x = logN, y = pctCore))+geom_point(colour = "turquoise")+
-    geom_line(aes(x = logN, y = CNpreds), color = "navy")+labs(x = "Log abundance", y = "% Core Occupancy")+
-    coord_cartesian(ylim = c(0, 1))
-  
-  #using exponential function since higher explanatory power than pwr function
-  #TA
-  TApreds =  coef_sub[,35]*(coef_sub[,18]) #35 = optimum; replacing ^ with * bc natural log, removing -1
-  plot1_3 = ggplot(coef_sub, aes(x = lnA, y = log(pctTran)))+geom_point(colour = "olivedrab")+
-    geom_line(aes(x = lnA, y = log(TApreds)), color = "navy") +labs(x = "Log area", y = "% Transient Occupancy")+
-    coord_cartesian(ylim = c(-4, 1)) #FIX
-  
-  #TN
-  TNpreds = coef_sub[,36]*(coef_sub[,22])
-  plot2_3 = ggplot(coef_sub, aes(x = lnN, y = log(pctTran)))+geom_point(colour = "olivedrab")+
-    geom_line(aes(x = lnN, y = TNpreds), color = "navy")+labs(x = "Log abundance", y = "% Transient Occupancy")+
-    coord_cartesian(ylim = c(-4, 1))
-  
-  #storing plots
-  predplot = grid.arrange(plot1, plot2, plot1_2, plot2_2, plot1_3, plot2_3,
-                          ncol=2, top = paste("predplot_", s, sep = ""))
-  #storing preds:
-  temp.df = data.frame(stateroute = s, logA = logA, 
-                       OApreds= OApreds , ONpreds = ONpreds, 
-                       CApreds = CApreds, CNpreds = CNpreds,
-                       TApreds = TApreds, TNpreds = TNpreds)
-  preds.df = rbind(preds.df, temp.df)
-  
-}
-dev.off()
-write.csv(preds.df, "C:/git/core-transient/scripts/R-scripts/scale_analysis/preds.csv", row.names = FALSE)
-
