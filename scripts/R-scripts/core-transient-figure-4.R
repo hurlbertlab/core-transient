5b8716398bbbaaeb9a7e5c74e89aba1efa533b30
ssnell6 <ssnell@ad.unc.edu>
2018-02-19 13:52:20 -0500

    working on predicted mods for plot

--- scripts/R-scripts/core-transient-figure-4.R
+++ scripts/R-scripts/core-transient-figure-4.R
@@ -81,7 +81,7 @@ bbs_occ = bbs_occ[!bbs_occ$site %in% c("53800-5-6", "53800-25-2"),]
 #### Fig 4c/d predicted model ####
 bbs_occ_pred = bbs_occ[!bbs_occ$datasetID %in% c(207, 210, 217, 218, 222, 223, 225, 238, 241, 248, 258, 282, 322, 280,317),]
 
-mod4c = lmer(pctTrans~(1|datasetID) * taxa * log10(meanAbundance), data=bbs_occ_pred)
+mod4c = lmer(pctTrans ~ log10(meanAbundance) * taxa + (log10(meanAbundance)|datasetID), data = bbs_occ_pred)
 summary(mod4c)
 occ_sub_pred = data.frame(datasetID = 999, taxa = unique(bbs_occ_pred$taxa), meanAbundance =  102) # 102 is median abun for data frame (median(bbs_occ_pred$meanAbundance))
 # to test: test = filter(occ_sub_pred, taxa == "Invertebrate")
@@ -100,7 +100,7 @@ predmod$order = c(1,4,3,6,7,5,2)
 
 # 4d
 ecosys = merge(bbs_occ_pred, dataformattingtable[,c("dataset_ID", "system")], by.y = "dataset_ID", by.x = "datasetID")
-mod4d = lmer(pctTrans~(1|datasetID) * system * log10(as.numeric(meanAbundance)), data=ecosys)
+mod4d = lmer(pctTrans ~ log10(meanAbundance) * system + (log10(meanAbundance)|datasetID), data=ecosys)
 summary(mod4d)
 occ_pred_4d = data.frame(datasetID = 999, system = unique(ecosys$system), meanAbundance =  102) # 102 is median abun for data frame (median(bbs_occ_pred$meanAbundance))
 predmod4d = merTools::predictInterval(mod4d, occ_pred_4d, n.sims=1000)
@@ -110,14 +110,14 @@ predmod4d$order = c(1:3)
 # pseudo r2 area
 bbs_occ_area = merge(bbs_occ_pred, areamerge[,c("datasetID", "site", "area")], by = c("datasetID", "site"))
 bbs_occ_area = na.omit(bbs_occ_area)
-mod4a = lmer(pctTrans~(1|datasetID) * taxa * log10(area), data=bbs_occ_area)
+mod4a = lmer(pctTrans ~ log10(area) * taxa + (log10(area)|datasetID), data=bbs_occ_area)
 r.squaredGLMM(mod4a)
 mod_a = lm(bbs_occ_area$pctTrans~predict(mod4a))
 summary(mod_a)
 rsquared(mod4a, aicc = FALSE)
 
 # pseudo r2 abun
-mod4b = lmer(pctTrans~(1|datasetID) * taxa * log10(meanAbundance), data=bbs_occ_area)
+mod4b = lmer(pctTrans ~ log10(meanAbundance) * taxa + (log10(meanAbundance)|datasetID), data = bbs_occ_area)
 mod_r = lm(bbs_occ_area$pctTrans~predict(mod4b))
 summary(mod_r)
 rsquared(mod4b, aicc = FALSE)
@@ -242,13 +242,99 @@ area_plot$slope = as.numeric(area_plot$slope)
 write.csv(area_plot, "output/tabular_data/fig_4a_output.csv", row.names =FALSE)
 
 
-# ggplot not happening
-#area_plot$taxa = factor(area_plot$taxa, levels = c('Invertebrate','Fish','Plankton','Mammal','Plant','Bird','Benthos'),ordered = TRUE)
-#colscale = c("gold2","turquoise2", "red", "purple4","forestgreen","#1D6A9B", "azure4")
 
-#p <- ggplot(area_plot, aes(x = log10(area), y = slope))
-#p + geom_abline(intercept = 0,slope = 1, lwd =1.5,linetype="dashed") + geom_point(aes(colour = taxa), size = 6) + xlab("Species Richness") + ylab("Species Richness Without Transients") + scale_colour_manual(breaks = plot_relationship$taxa,values = colscale) + theme(axis.text.x=element_text(size=24),axis.text.y=element_text(size=24),axis.title.x=element_text(size=32),axis.title.y=element_text(size=32,angle=90,vjust = 2))+ theme_classic()
-#ggsave(file="C:/Git/core-transient/output/plots/sparea_4c.pdf", height = 10, width = 15)
+
+###### panel plot pred model ######
+pdf('output/plots/4a_4d_pred.pdf', height = 10, width = 14)
+par(mfrow = c(2, 2), mar = c(5,5,1,1), cex = 1, oma = c(0,0,0,0), las = 1)
+palette(colors7)
+
+areamerge_fig = na.omit(areamerge_fig)
+all = lmer(pctTrans ~ log10(area) * taxa + (log10(area) | datasetID), data = bbs_occ_area)
+
+xnew = range(log10(areamerge_fig$area))
+xhat <- predict(all, newdata = data.frame((xnew)))
+xhats = range(xhat)
+lower = range(xhat)[1]
+upper = range(xhat)[2]
+
+
+plot(NA, xlim = c(-2, 8), ylim = c(0,1), xlab = expression("log"[10]*" Area (m"^2*")"), 
+     ylab = "% Transients", cex.lab = 2, frame.plot=FALSE, xaxt = "n", yaxt = "n", 
+     mgp = c(3.25,1,0))
+axis(1, cex.axis =  1.5)
+axis(2, cex.axis =  1.5)
+b1 = for(id in scaleIDs){
+  print(id)
+  plotsub = subset(areamerge_fig,datasetID == id)
+  taxa = as.character(unique(plotsub$taxa))
+  mod4 = lmer(pctTrans ~ log10(area) * taxa + (log10(area) | datasetID), data = plotsub)
+  mod4.slope = summary(mod4)$coef[2,"Estimate"]
+  mod4.coef1 = summary(mod4$coef[1])[3]
+  xnew = range(log10(plotsub$area))
+  xhat <- predict(mod4, newdata = data.frame((xnew)))
+  xhats = range(xhat)
+  lower = range(xhat)[1]
+  upper = range(xhat)[2]
+  print(xhats)
+  taxcolor = subset(taxcolors, taxa == as.character(plotsub$taxa)[1])
+  y= summary(mod4)$coef[1]+ (xhats)*summary(mod4)$coef[2]
+  area_plot  = rbind(area_plot , c(id, lower,upper, mod4.slope,taxa))
+  lines(log10(plotsub$area), fitted(mod4), col=as.character(taxcolor$color),lwd=4)
+  # points(log10(plotsub$area), plotsub$pctTrans)
+  par(new=TRUE)
+}
+lines(log10(areamerge_fig$area), fitted(all), col="black", lwd=3)
+title(outer=FALSE,adj=0.02,main="A",cex.main=2,col="black",font=2,line=-1)
+par(new= FALSE)
+
+
+bbs_occ = na.omit(bbs_occ)
+occ_all = lm(bbs_occ$pctTrans ~ log10(bbs_occ$meanAbundance))
+xnew = range(log10(bbs_occ$meanAbundance))
+xhat <- predict(occ_all, newdata = data.frame((xnew)))
+xhats = range(xhat)
+
+plot(NA, xlim = c(0, 7), ylim = c(0,1), col = as.character(taxcolor$color), xlab = expression("log"[10]*" Community Size"), ylab = "% Transients", cex.lab = 2,frame.plot=FALSE, yaxt = "n", xaxt = "n", mgp = c(3.25,1,0))
+axis(1, cex.axis =  1.5)
+axis(2, cex.axis =  1.5)
+b2 = for(id in scaleIDs){
+  print(id)
+  plotsub = subset(bbs_occ,datasetID == id)
+  mod4 = lm(plotsub$pctTrans ~ log10(plotsub$meanAbundance))
+  xnew = range(log10(plotsub$meanAbundance))
+  xhat <- predict(mod4, newdata = data.frame((xnew)))
+  xhats = range(xhat)
+  print(xhats)
+  taxcolor = subset(taxcolors, taxa == as.character(plotsub$taxa)[1])
+  y=summary(mod4)$coef[1] + (xhats)*summary(mod4)$coef[2]
+  lines(log10(plotsub$meanAbundance), fitted(mod4), col=as.character(taxcolor$color),lwd=4)
+  # points(log10(plotsub$meanAbundance), plotsub$pctTrans)
+  par(new=TRUE)
+}
+abline(v = log10(102), lty = 'dotted', lwd = 2) 
+par(new=TRUE)
+title(outer=FALSE,adj=0.02,main="B",cex.main=2,col="black",font=2,line=-1)
+lines(log10(bbs_occ$meanAbundance), fitted(occ_all), col="black",lwd=3)
+legend('topright', legend = as.character(taxcolors$taxa), lty=1,lwd=3,col = as.character(taxcolors$color), cex = 1.5, bty = "n")
+par(new = FALSE)
+
+b4 = barplot(predmod$fit[predmod$order], cex.names = 2,col = c(colors()[17],"gold2", "turquoise2","red","forestgreen","purple4","#1D6A9B"), ylim = c(0, 1.1), yaxt = "n")
+axis(2, cex.axis = 1.5)
+Hmisc::errbar(c(0.7, 1.9, 3.1, 4.3, 5.5, 6.7, 7.9), predmod$fit[predmod$order], predmod$upr[predmod$order], predmod$lwr[predmod$order], add= TRUE, lwd = 1.25, pch = 3)
+mtext("% Transients", 2, cex = 2, las = 0, line = 3, mgp = c(3.25,1,0))
+title(outer=FALSE,adj=0.02,main="C",cex.main=2,col="black",font=2,line=-1)
+
+b4 = barplot(predmod4d$fit[predmod4d$order], cex.names = 1.5,col = c('burlywood','skyblue','navy'), ylim = c(0, 0.8), yaxt = "n")
+axis(2, cex.axis = 1.5)
+Hmisc::errbar(c(0.7, 1.9, 3.1), predmod4d$fit[predmod4d$order], predmod4d$upr[predmod4d$order], predmod4d$lwr[predmod4d$order], add= TRUE, lwd = 1.25, pch = 3)
+mtext("% Transients", 2, cex = 2, las = 0, line = 3, mgp = c(3.25,1,0))
+title(outer=FALSE,adj=0.02,main="D",cex.main=2,col="black",font=2,line=-1)
+dev.off()
+
+dev.off()
+
+###### ######
 
 # pseudo r2
 mod4 = lm(bbs_occ$pctTrans ~ log10(bbs_occ$meanAbundance))
