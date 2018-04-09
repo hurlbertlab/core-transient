84825928d829770be0fbd7e503f2e7894a78ee67
Molly Jenkins <mollyfrn@live.unc.edu>
2018-04-09 12:50:16 -0400

    co-opting old enec563 hw for help

--- scripts/R-scripts/scale_analysis/rollmeans_SEcalcs.R
+++ scripts/R-scripts/scale_analysis/rollmeans_SEcalcs.R
@@ -5,23 +5,7 @@ bbs_allscales = bbs_allscales %>%
   dplyr::filter(logN != "NA")
 
 
-#do I calc SE's using a rollapply function at the same time I calc mean, or do I do this after? 
-#the former: 
-central_altA = bbs_allscales %>%  
-  dplyr::select(logA, pctCore) %>% 
-  transmute(pctCore_m = rollapply(pctCore, width = 1, FUN = mean, na.rm = TRUE, fill = NULL),
-            pctCore_sd = rollapply(pctCore, width = 1, FUN = sd, na.rm = TRUE, fill = NULL),
-            logA = logA) %>% 
-  mutate(logA = round(logA, digits = 2)) %>%
-  group_by(logA) %>%
-  summarise(pctCore = mean(pctCore_m),
-            sdC = mean(pctCore_sd)) %>% 
-  mutate(focalrte = "99999", logA = logA) %>% #add dummy focal rte code for plotting mean 
-  dplyr::select(focalrte, logA, pctCore, sdC)
-
-
-
-#the latter: 
+#do I calc SE's using a rollapply-esque function at the same time I calc mean, or do I do this after? 
 central_altB = bbs_allscales %>%  
   dplyr::select(logA, pctCore) %>% 
   transmute(pctCore_m = rollapply(pctCore, width = 1, FUN = mean, na.rm = TRUE, fill = NULL),
@@ -29,6 +13,17 @@ central_altB = bbs_allscales %>%
   mutate(logA = round(logA, digits = 2)) %>%
   group_by(logA) %>%
   summarise(pctCore = mean(pctCore_m),
-            sdC = sd(pctCore_m)) %>% 
+            sdC = sd(pctCore_m)) %>% #calc sd for rolled avgs bc if try to for pctCore, just get same # repeated
   mutate(focalrte = "99999", logA = logA) %>% 
   dplyr::select(focalrte, logA, pctCore, sdC)
+
+
+
+parests = pctCore #mean pctcore estimates
+vcov(mod1)
+par.se = sd(x)/sqrt(sum(!is.na(x)))
+
+
+par.se <- sqrt(diag(vcov(mod1)))
+upper95 <- pctCore+par.se*qt(.975,df.residual(mod1))
+lower95 <- pctCore-par.se*qt(.975,df.residual(mod1))
