82b8bb89fd9be6c8851b1f473ae851e4613db252
ssnell6 <ssnell@ad.unc.edu>
2018-05-10 10:17:18 -0400

    changed script names

--- scripts/R-scripts/analysis.r
+++
@@ -1,9 +0,0 @@
-# Code for producing simpler summaries from data_formatting_table
-dataFormattingTable = read.csv('data_formatting_table.csv', header=T)
-
-#Site notes
-subsetNotes = function(dataFormattingTable, subsetCols) {
-  d2 = dataFormattingTable[dataFormattingTable$format_flag == 1,]
-  d2[order(d2$taxa, d2$dataset_ID), c('dataset_ID', 'dataset_name', 'taxa', 'location', subsetCols)]
-}
-
